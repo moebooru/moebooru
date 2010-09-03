78aaa23aeb13677cabef1c58b9d088b4f8567efa
petopeto <none@none>
2010-09-03 22:01:27 +0000

    remove master/slave columns
    --HG--
    branch : moe
    extra : convert_revision : svn%3A2d28d66d-8d94-df11-8c86-00306ef368cb/trunk/moe%4086

---
+++ db/migrate/20100903220234_remove_slave_pool_posts.rb
@@ -0,0 +1,11 @@
+class RemoveSlavePoolPosts < ActiveRecord::Migration
+  def self.up
+    execute "ALTER TABLE pools_posts DROP COLUMN master_id"
+    execute "ALTER TABLE pools_posts DROP COLUMN slave_id"
+  end
+
+  def self.down
+    execute "ALTER TABLE pools_posts ADD COLUMN master_id INTEGER REFERENCES pools_posts ON DELETE SET NULL"
+    execute "ALTER TABLE pools_posts ADD COLUMN slave_id INTEGER REFERENCES pools_posts ON DELETE SET NULL"
+  end
+end

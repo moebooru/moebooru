787ea5b7386e02475d7ccc62b1b1f3476395982b
petopeto <none@none>
2010-09-07 02:42:53 +0000

    r1817, allow setting limit for reports
    --HG--
    branch : moe
    extra : convert_revision : svn%3A2d28d66d-8d94-df11-8c86-00306ef368cb/trunk/moe%40129

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -3,28 +3,28 @@ class ReportController < ApplicationController
   before_filter :set_dates
 
   def tag_updates
-    @users = Report.tag_updates(@start_date, @end_date)
+    @users = Report.tag_updates(@start_date, @end_date, @limit)
     @report_title = "Tag Updates"
     @change_params = lambda {|user_id| {:controller => "post_tag_history", :action => "index", :user_id => user_id}}
     render :action => "common"
   end
 
   def note_updates
-    @users = Report.note_updates(@start_date, @end_date)
+    @users = Report.note_updates(@start_date, @end_date, @limit)
     @report_title = "Note Updates"
     @change_params = lambda {|user_id| {:controller => "note", :action => "history", :user_id => user_id}}
     render :action => "common"
   end
 
   def wiki_updates
-    @users = Report.wiki_updates(@start_date, @end_date)
+    @users = Report.wiki_updates(@start_date, @end_date, @limit)
     @report_title = "Wiki Updates"
     @change_params = lambda {|user_id| {:controller => "wiki", :action => "recent_changes", :user_id => user_id}}
     render :action => "common"
   end
 
   def post_uploads
-    @users = Report.wiki_updates(@start_date, @end_date)
+    @users = Report.wiki_updates(@start_date, @end_date, @limit)
     @report_title = "Post Uploads"
     @change_params = lambda {|user_id| {:controller => "post", :action => "index", :tags => "user:#{User.find_name(user_id)}"}}
     render :action => "common"
@@ -77,6 +77,16 @@ private
       @end_date = Date.parse(params[:end_date])
     else
       @end_date = Date.today
+    end
+
+    if params[:limit]
+      @limit = params[:limit].to_i
+    else
+      @limit = 29
+    end
+
+    if @limit > 100
+      @limit = 100
     end
   end
 end
--- lib/report.rb
+++ lib/report.rb
@@ -1,10 +1,10 @@
 module Report
-  def usage_by_user(table_name, start, stop, conds = [], params = [], column = "created_at")
+  def usage_by_user(table_name, start, stop, conds = [], params = [], column = "created_at", limit = 29)
     conds << ["%s BETWEEN ? AND ?" % column]
     params << start
     params << stop
 
-    users = ActiveRecord::Base.select_all_sql("SELECT user_id, COUNT(*) as change_count FROM #{table_name} WHERE " + conds.join(" AND ") + " GROUP BY user_id ORDER BY change_count DESC LIMIT 29", *params)
+    users = ActiveRecord::Base.select_all_sql("SELECT user_id, COUNT(*) as change_count FROM #{table_name} WHERE " + conds.join(" AND ") + " GROUP BY user_id ORDER BY change_count DESC LIMIT #{limit}", *params)
 
     conds << "user_id NOT IN (?)"
     params << users.map {|x| x["user_id"]}
@@ -25,24 +25,27 @@ module Report
     return add_sum(users)
   end
   
-  def tag_updates(start, stop)
-    users = usage_by_user("post_tag_histories", start, stop)
+  def tag_updates(start, stop, limit)
+    users = usage_by_user("post_tag_histories", start, stop, limit)
     users.each do |user|
-      user["change_count"] = user["change_count"] - Post.count(:all, :conditions => ["created_at BETWEEN ? AND ? AND user_id = ? AND status = 'active'", start, stop, user["user_id"]])
+      user["change_count"] = user["change_count"] - Post.count(:all, :conditions => ["created_at BETWEEN ? AND ? AND user_id =?", start, stop, user["user_id"]])
     end
+    bottom = users.pop
+    users.sort! {|a, b| b["change_count"] <=> a["change_count"]}
+    users.push(bottom)
     users
   end
 
-  def post_uploads(start, stop)
-    usage_by_user("posts", start, stop)
+  def post_uploads(start, stop, limit)
+    usage_by_user("posts", start, stop, limit)
   end
 
-  def wiki_updates(start, stop)
-    usage_by_user("wiki_page_versions", start, stop)
+  def wiki_updates(start, stop, limit)
+    usage_by_user("wiki_page_versions", start, stop, limit)
   end
 
-  def note_updates(start, stop)
-    usage_by_user("note_versions", start, stop)
+  def note_updates(start, stop, limit)
+    usage_by_user("note_versions", start, stop, limit)
   end
 
   def add_sum(users)

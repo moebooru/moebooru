8e91026539bdd941fe2795acb53354606b054246
petopeto <none@none>
2010-09-07 03:35:20 +0000

    missed adding a migration
    --HG--
    branch : moe
    extra : convert_revision : svn%3A2d28d66d-8d94-df11-8c86-00306ef368cb/trunk/moe%40156

---
+++ db/migrate/20090123212834_add_visible_on_profile_to_tag_subscriptions.rb
@@ -0,0 +1,23 @@
+class AddVisibleOnProfileToTagSubscriptions < ActiveRecord::Migration
+  def self.up
+    remove_index :favorite_tags, :name
+    remove_index :favorite_tags, :user_id
+    remove_foreign_key :favorite_tags, "favorite_tags_user_id_fkey"
+    rename_table :favorite_tags, :tag_subscriptions
+    add_index :tag_subscriptions, :name
+    add_index :tag_subscriptions, :user_id
+    add_foreign_key :tag_subscriptions, :user_id, :users, :id, :on_delete => :cascade
+    add_column :tag_subscriptions, :is_visible_on_profile, :boolean, :null => false, :default => true
+  end
+
+  def self.down
+    remove_column :tag_subscriptions, :is_visible_on_profile
+    remove_foreign_key :tag_subsriptions, "tag_subscriptions_user_id_fkey"
+    remove_index :tag_subscriptions, :user_id
+    remove_index :tag_subscriptions, :name
+    rename_table :tag_subscriptions, :favorite_tags
+    add_foreign_key :favorite_tags, :user_id, :users, :id, :on_delete => :cascade
+    add_index :favorite_tags, :user_id
+    add_index :favorite_tags, :name
+  end
+end

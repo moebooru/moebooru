bfb3b501c44a06979fe28afd4eff62763cecaf7c
petopeto <none@none>
2010-09-07 04:26:02 +0000

    fix fav tags job task isn't renamed to tag subscriptions
    --HG--
    branch : moe
    extra : convert_revision : svn%3A2d28d66d-8d94-df11-8c86-00306ef368cb/trunk/moe%40162

---
+++ db/migrate/20100907042612_rename_favorites_job_task.rb
@@ -0,0 +1,10 @@
+class RenameFavoritesJobTask < ActiveRecord::Migration
+  def self.up
+    execute "UPDATE job_tasks SET task_type='calculate_tag_subscriptions' WHERE task_type='calculate_favorite_tags'"
+  end
+
+  def self.down
+    execute "UPDATE job_tasks SET task_type='calculate_favorite_tags' WHERE task_type='calculate_tag_subscriptions'"
+  end
+end
+

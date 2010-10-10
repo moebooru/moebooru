ebf8aed70c5aec98cb2bdb1518c09e7847a3bfba
petopeto <none@none>
2010-10-10 05:11:43 +0000

    avoid reopening geoip database for every lookup
    --HG--
    branch : moe
    extra : convert_revision : svn%3A2d28d66d-8d94-df11-8c86-00306ef368cb/trunk/moe%40233

--- lib/geoip.rb
+++ lib/geoip.rb
@@ -4,14 +4,17 @@ module GeoIP
   # Given an IP, return a two-letter country code.  If the IP isn't known or the GeoIP
   # database isn't available, return "??".
   def get_country_for_ip(ip)
-    fn = "#{RAILS_ROOT}/vendor/plugins/net-geoip/GeoIP.dat"
+    if @db.nil? then
+      fn = "#{RAILS_ROOT}/vendor/plugins/net-geoip/GeoIP.dat"
 
-    # GeoIP.open() will show an error if the file doesn't exist, and then return an instance
-    # anyway and crash when we try to use it.  Make sure it exists.
-    return "??" if not File.exists?(fn)
+      # GeoIP.open() will show an error if the file doesn't exist, and then return an instance
+      # anyway and crash when we try to use it.  Make sure it exists.
+      return "??" if not File.exists?(fn)
 
-    g = Net::GeoIP.open(fn)
-    country = g.country_code_by_addr(ip)
+      @db = Net::GeoIP.open(fn)
+    end
+
+    country = @db.country_code_by_addr(ip)
     return "??" if country.nil?
     return country
   end

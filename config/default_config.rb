0d4a379e4ad3b6718b5e9f5e7290783ae382fa80
petopeto <none@none>
2010-10-13 02:37:11 +0000

    zh -> actual language code zh-CN
    --HG--
    branch : moe
    extra : convert_revision : svn%3A2d28d66d-8d94-df11-8c86-00306ef368cb/trunk/moe%40257

--- config/default_config.rb
+++ config/default_config.rb
@@ -270,7 +270,7 @@ CONFIG["max_tag_subscriptions"] = 5
 
 # Languages that we support translating to.  We'll translate each comment into all of these
 # languages.  Set this to nil to disable translation.
-CONFIG["translate_languages"] = ["en", "ja", "zh", "es"]
+CONFIG["translate_languages"] = ["en", "ja", "zh-CN", "es"]
 
 # Load expanded language names.
 require 'languages'

c5a8e9a2a3d36ccb5d170d18341e154fceb349c7
petopeto <none@none>
2010-10-15 00:22:04 +0000

    merge from r1792 (we don't actually use favorite_controller anymore, just syncing)
    --HG--
    branch : moe
    extra : convert_revision : svn%3A2d28d66d-8d94-df11-8c86-00306ef368cb/trunk/moe%40277

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -9,6 +9,7 @@ class FavoriteController < ApplicationController
     respond_to do |fmt|
       fmt.json do
         render :json => {:favorited_users => @post.favorited_by.map(&:name).join(",")}.to_json
+        render :xml => {:favorited_users => @post.favorited_by.map(&:name)}.to_xml
       end
       fmt.xml do
         render :xml => {:favorited_users => @post.favorited_by.map(&:name)}.to_xml

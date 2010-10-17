5adcef9629fdc1311f3c0d618d9fc0b032c86405
petopeto <none@none>
2010-10-17 03:56:00 +0000

    fixup weird migration: 046 discards db exceptions, which doesn't work with postgres, and 050 does something expecting an error and discards it which also doesn't work with postgres; not sure what this was trying to work around but it doesn't look necessary
    --HG--
    branch : moe
    extra : convert_revision : svn%3A2d28d66d-8d94-df11-8c86-00306ef368cb/trunk/moe%40295

--- db/migrate/046_posts_tags_add_foreign_keys.rb
+++ db/migrate/046_posts_tags_add_foreign_keys.rb
@@ -4,15 +4,8 @@ class PostsTagsAddForeignKeys < ActiveRecord::Migration
       execute "update tags set post_count = (select count(*) from posts_tags pt where pt.tag_id = tags.id)"
       execute "update tags set safe_post_count = (select count(*) from posts_tags pt, posts p where pt.tag_id = tags.id and pt.post_id = p.id and p.rating = 's')"
 
-      begin
-        execute "alter table posts_tags add constraint fk_posts_tags__post foreign key (post_id) references posts on delete cascade"
-      rescue Exception
-      end
-
-      begin
-        execute "alter table posts_tags add constraint fk_posts_tags__tag foreign key (tag_id) references tags on delete cascade"
-      rescue Exception
-      end
+      execute "alter table posts_tags add constraint fk_posts_tags__post foreign key (post_id) references posts on delete cascade"
+      execute "alter table posts_tags add constraint fk_posts_tags__tag foreign key (tag_id) references tags on delete cascade"
     end
   end
 

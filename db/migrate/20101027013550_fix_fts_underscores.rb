3d345d40aab5189f935423023cf7f1f2c318dc65
petopeto <none@none>
2010-10-27 05:45:21 +0000

    fix the pool search getting confused over the underscores in normalized pool titles
    --HG--
    branch : moe
    extra : convert_revision : svn%3A2d28d66d-8d94-df11-8c86-00306ef368cb/trunk/moe%40299

---
+++ db/migrate/20101027013550_fix_fts_underscores.rb
@@ -0,0 +1,32 @@
+class  FixFtsUnderscores < ActiveRecord::Migration
+  def self.up
+    execute """
+      CREATE OR REPLACE FUNCTION replace_underscores(s varchar) RETURNS varchar IMMUTABLE AS $$
+        BEGIN
+          RETURN regexp_replace(s, '_', ' ', 'g');
+        END;
+      $$ LANGUAGE plpgsql;
+    """
+
+    # The name column has spaces replaced with underscores (for some reason).  Change them back
+    # to spaces before passing it to to_tsvector, so it doesn't confuse the parser; for example,
+    # things like "foo._bar" are parsed as filenames.
+    execute """
+      CREATE OR REPLACE FUNCTION pools_search_update_trigger() RETURNS trigger AS $$
+      BEGIN
+        new.search_index := to_tsvector('pg_catalog.english', replace_underscores(new.name) || ' ' || new.description);
+        RETURN new;
+      END
+      $$ LANGUAGE plpgsql;
+    """
+
+    # Trigger updates.
+    execute "UPDATE pools SET name=name;"
+  end
+
+  def self.down
+    execute "DROP FUNCTION replace_underscores"
+  end
+end
+     
+

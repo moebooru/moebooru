a2176982d0830b5370e4b351ef6ab9a232e95901
petopeto <none@none>
2010-11-17 22:49:53 +0000

    route for browse-pool
    --HG--
    branch : moe
    extra : convert_revision : svn%3A2d28d66d-8d94-df11-8c86-00306ef368cb/trunk/moe%40427

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect "", :controller => "static", :action => "index"
   map.connect "post/show/:id/:tag_title", :controller => "post", :action => "show", :requirements => {:id => /\d+/}
+  map.connect "post/browse-pool/:id", :controller => "post", :action => "browse-pool", :requirements => {:id => /\d+/}
   map.connect "pool/zip/:id/:filename", :controller => "pool", :action => "zip", :requirements => {:id => /\d+/, :filename => /.*/}
   map.connect ":controller/:action/:id.:format", :requirements => {:id => /[-\d]+/}
   map.connect ":controller/:action/:id", :requirements => {:id => /[-\d]+/}

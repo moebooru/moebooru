5a104aad09667c64939314bbd87cb253a4cf61df
petopeto <none@none>
2010-11-19 04:45:56 +0000

    use document.documentElement.clientWidth/Height to handle scrollbars correctly
    --HG--
    branch : moe
    extra : convert_revision : svn%3A2d28d66d-8d94-df11-8c86-00306ef368cb/trunk/moe%40460

--- public/javascripts/post.js
+++ public/javascripts/post.js
@@ -697,8 +697,8 @@ Post = {
     if(padding_top < 0) padding_top = 0;
     img.setStyle({paddingTop: padding_top + "px"});
 
-    var required_width = target_offset[0] + window.innerWidth;
-    var required_height = target_offset[1] + window.innerHeight;
+    var required_width = target_offset[0] + document.documentElement.clientWidth;
+    var required_height = target_offset[1] + document.documentElement.clientHeight;
     $(document.body).setStyle({minWidth: required_width + "px", minHeight: required_height + "px"});
 
     /* Resizing the body may shift the image to the right, since it's centered in the content.
@@ -718,8 +718,8 @@ Post = {
       img.original_width = img.width;
       img.original_height = img.height;
     }
-    var client_width = window.innerWidth;
-    var client_height = window.innerHeight;
+    var client_width = document.documentElement.clientWidth;
+    var client_height = document.documentElement.clientHeight;
 
     /* Zoom the image to fit the viewport. */
     var ratio = client_width / img.original_width;

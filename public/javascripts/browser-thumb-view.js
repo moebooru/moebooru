7a48f5f74c52cb5251afdc91e706685bf6ad87c8
petopeto <none@none>
2010-11-20 22:38:37 +0000

    post browse stuff
    --HG--
    branch : moe
    extra : convert_revision : svn%3A2d28d66d-8d94-df11-8c86-00306ef368cb/trunk/moe%40481

--- public/javascripts/browser-thumb-view.js
+++ public/javascripts/browser-thumb-view.js
@@ -2,11 +2,16 @@ ThumbnailView = function(container, post_ids, pool)
 {
   this.container = container;
   this.post_ids = post_ids;
-  this.focused_post_id = null;
+  this.expanded_post_id = null;
+  this.centered_post_id = null;
   this.pool = pool;
+  this.last_mouse_x = 0;
+  this.last_mouse_y = 0;
+  this.thumb_container_shown = true;
 
   this.hashchange_event = this.hashchange_event.bindAsEventListener(this);
   this.thumbnail_click_event = this.thumbnail_click_event.bindAsEventListener(this);
+  this.thumbnail_dblclick_event = this.thumbnail_dblclick_event.bindAsEventListener(this);
   this.mouse_wheel_event = this.mouse_wheel_event.bindAsEventListener(this);
 
   this.container.observe("DOMMouseScroll", this.mouse_wheel_event);
@@ -14,17 +19,26 @@ ThumbnailView = function(container, post_ids, pool)
 
   Post.observe_finished_loading(this.displayed_image_finished_loading.bind(this));
 
+  OnKey(65, { AlwaysAllowOpera: true, allowRepeat: true }, function(e) { this.show_next_post(true); }.bind(this));
+  OnKey(83, { AlwaysAllowOpera: true, allowRepeat: true }, function(e) { this.show_next_post(false); }.bind(this));
+//  OnKey(32, { AlwaysAllowOpera: true }, function(e) { this.toggle_thumb_bar(); }.bind(this));
 
-  OnKey(65, { AlwaysAllowOpera: true, allowRepeat: true }, function(e) { this.scroll(true, false); }.bind(this));
-  OnKey(83, { AlwaysAllowOpera: true, allowRepeat: true }, function(e) { this.scroll(false, false); }.bind(this));
+  /* We need to watch for space presses during keypress rather than keydown, since
+   * for some reason cancelling during the keydown event won't prevent the browser
+   * default behavior like it probably should. */
+  this.document_keypress_event = this.document_keypress_event.bindAsEventListener(this);
+  Element.observe(document, "keypress", this.document_keypress_event);
 
   this.ignore_hash_changes_until = 0;
   Element.observe(window, "hashchange", this.hashchange_event);
 
-  document.observe("mouseout", function(e) { if(e.relatedTarget != null) return; this.set_visibility_from_mouse_pos(0, 1000000); }.bindAsEventListener(this));
-  
   this.document_mousemove_event = this.document_mousemove_event.bindAsEventListener(this);
-  document.observe("mousemove", this.document_mousemove_event);
+  this.container.observe("mousemove", this.document_mousemove_event);
+
+  this.container_mouseover_event = this.container_mouseover_event.bindAsEventListener(this);
+  this.container_mouseout_event = this.container_mouseout_event.bindAsEventListener(this);
+  this.container.observe("mouseover", this.container_mouseover_event);
+  this.container.observe("mouseout", this.container_mouseout_event);
 
   for(var i = 0; i < post_ids.length; ++i)
   {
@@ -34,9 +48,38 @@ ThumbnailView = function(container, post_ids, pool)
 
   var initial_post_id = this.get_current_post_id();
 
+  this.center_on_post(initial_post_id);
   this.focus_post(initial_post_id);
-  pool_browser.set_post(initial_post_id);
+  this.set_active_post(initial_post_id);
 }
+
+ThumbnailView.prototype.container_mouseover_event = function(event)
+{
+  var li = event.target.up(".post-thumb");
+  if(!li)
+    return;
+  var post_id = li.post_id;
+
+  this.expand_post(post_id);
+}
+
+ThumbnailView.prototype.container_mouseout_event = function(event)
+{
+}
+
+ThumbnailView.prototype.document_keypress_event = function(e) {
+  var key = e.charCode;
+  if(key == null)
+    key = e.keyCode; /* Opera */
+  if (key != 32)
+    return;
+  if (e.shiftKey || e.altKey || e.ctrlKey || e.metaKey)
+    return;
+
+  this.toggle_thumb_bar();
+  e.stop();
+}
+
 
 ThumbnailView.prototype.hashchange_event = function(e)
 {
@@ -57,8 +100,9 @@ ThumbnailView.prototype.hashchange_event = function(e)
   if((new Date()).valueOf() < this.ignore_hash_changes_until)
     return;
 
+  this.center_on_post(new_post_id);
   this.focus_post(new_post_id);
-  pool_browser.set_post(new_post_id);
+  this.set_active_post(new_post_id);
 }
 
 ThumbnailView.prototype.get_current_post_id = function()
@@ -74,38 +118,11 @@ ThumbnailView.prototype.get_current_post_id = function()
   return post_id;
 }
 
-ThumbnailView.prototype.set_visibility_from_mouse_pos = function(x, y)
-{
-  var offset = this.container.cumulativeOffset();
-  var fade_distance = 150.0;
-  var box_top = offset.top;
-  var box_bottom = 350;
-
-  var a;
-  if(y < box_top)
-  {
-    var d = box_top - y;
-    a = (fade_distance-d)/fade_distance;
-  }
-  else if(y >= box_bottom)
-  {
-    var d = y - box_bottom;
-    a = (fade_distance-d)/fade_distance;
-  }
-  else
-  {
-    a = 1;
-  }
-
-  if(a < 0) a = 0;
-  if(a > 1) a = 1;
-
-  this.container.setStyle({top: -(1-a)*box_bottom + "px"});
-
-}
-
 ThumbnailView.prototype.document_mousemove_event = function(e) {
-  this.set_visibility_from_mouse_pos(e.pointerX(), e.pointerY() - document.documentElement.scrollTop);
+  var x = e.pointerX();
+  var y = e.pointerY() - document.documentElement.scrollTop;
+  this.last_mouse_x = x;
+  this.last_mouse_y = y;
 }
 
 ThumbnailView.prototype.mouse_wheel_event = function(event)
@@ -135,12 +152,47 @@ ThumbnailView.prototype.post_id_idx = function(post_id)
   return -1;
 }
 
-ThumbnailView.prototype.scroll = function(up, lazy)
+ThumbnailView.prototype.set_active_post = function(post_id, lazy)
 {
-  var current_post_id = this.focused_post_id;
-  if(current_post_id == null)
-    current_post_id = this.post_ids[0];
+  this.active_post_id = post_id;
 
+  if(lazy)
+  {
+    /* Ask the pool browser to load the new post, with a delay in case we're
+     * scrolling quickly. */
+    pool_browser.lazily_load(post_id);
+  } else {
+    pool_browser.set_post(post_id);
+  }
+}
+
+ThumbnailView.prototype.show_next_post = function(up)
+{
+  var active_post_id = this.active_post_id;
+  var current_idx = this.post_id_idx(active_post_id);
+  var new_idx = current_idx + (up? -1:+1);
+
+  if(new_idx < 0)
+  {
+    notice("Continued from the end");
+    new_idx = this.post_ids.length - 1;
+  }
+  else if(new_idx >= this.post_ids.length)
+  {
+    notice("Starting over from the beginning");
+    new_idx = 0;
+  }
+
+  var new_post_id = this.post_ids[new_idx];
+  this.center_on_post(new_post_id);
+  this.expand_post(new_post_id);
+  this.set_active_post(new_post_id, true);
+}
+
+/* Scroll the thumbnail view left or right.  Don't change the displayed post. */
+ThumbnailView.prototype.scroll = function(up)
+{
+  var current_post_id = this.centered_post_id;
   var current_idx = this.post_id_idx(current_post_id);
   var new_idx = current_idx + (up? -1:+1);
 
@@ -156,34 +208,45 @@ ThumbnailView.prototype.scroll = function(up, lazy)
     notice("Starting over from the beginning");
     new_idx = 0;
   }
-  new_idx = (new_idx + this.post_ids.length) % this.post_ids.length;
 
   var new_post_id = this.post_ids[new_idx];
-  this.focus_post(new_post_id);
+  this.center_on_post_for_scroll(new_post_id);
+}
 
-  if(lazy)
+/* This calls center_on_post, and also unexpands and reexpands posts correctly to avoid flicker. */
+ThumbnailView.prototype.center_on_post_for_scroll = function(post_id)
+{
+  /*
+   * Centering on a new post is likely to change which one we're hovering over.  Unexpand
+   * the current post before scrolling, so the post doesn't flicker expanded in a new position
+   * before being un-expanded by the mouseover event.
+   */
+  this.expand_post(null);
+
+  this.center_on_post(post_id);
+
+  /*
+   * Now that we've re-centered, we need to expand the correct image.  Usually, we can just
+   * wait for the mouseover event to fire, which will expand the element we're hovering over
+   * after centering on the new post.  However, in some cases we won't actually change which
+   * one we're hovering over, eg. if the mouse is over the edge of a landscape image, and
+   * after scrolling the mouse is centered over the same image.  Explicitly figure out which
+   * item we're hovering over and expand it.
+   */
+  var element = document.elementFromPoint(this.last_mouse_x, this.last_mouse_y);
+  if(element)
   {
-    /* Ask the pool browser to load the new post, with a delay in case we're
-     * scrolling quickly. */
-    pool_browser.lazily_load(new_post_id);
-  } else {
-    pool_browser.set_post(new_post_id);
+    var li = element.up(".post-thumb");
+    if(li)
+    {
+      var post_id = li.post_id;
+      this.expand_post(post_id);
+    }
   }
 }
 
-ThumbnailView.prototype.focus_post = function(post_id)
+ThumbnailView.prototype.center_on_post = function(post_id)
 {
-  if(post_id == this.focused_post_id)
-    return;
-
-  if(this.focused_post_id != null)
-  {
-    var old_thumb = $("p" + this.focused_post_id);
-    old_thumb.down(".inner").setStyle({overflow: "hidden"});
-    old_thumb.setStyle({zIndex: 0});
-    this.focused_post_id = null;
-  }
-
   /* 
    * We have to jump some hoops to scroll the thumbs correctly.  We should be able to
    * set the container to overflow-x: hidden and just change scrollLeft to scroll the
@@ -198,19 +261,18 @@ ThumbnailView.prototype.focus_post = function(post_id)
    */
   var padding = this.container.down(".post-padding");
 
-  /* Clear the padding before calculating the new padding. */
-  padding.setStyle({marginLeft: 0});
-
   var thumb = $("p" + post_id);
   var img = thumb.down(".preview");
-
-  var window_left = document.documentElement.scrollLeft;
-  var window_top = document.documentElement.scrollTop;
 
   /* We always center the thumb.  Don't clamp to the edge when we're near the first or last
    * item, so we always have empty space on the sides for expanded landscape thumbnails to
    * be visible. */
-  var shift_pixels_right = this.container.offsetWidth/2 - img.width/2 - img.cumulative_offset_range_x(img.up("UL"));
+  var center_on_position = this.container.offsetWidth/2;
+
+  /* Clear the padding before calculating the new padding. */
+  padding.setStyle({marginLeft: 0});
+
+  var shift_pixels_right = center_on_position - img.width/2 - img.cumulative_offset_range_x(img.up("UL"));
 
   /* As of build 3516 (10.63), Opera is handling offsetLeft for elements inside fixed containers
    * incorrectly: they're affected by the scroll position, even though the nodes aren't.  This
@@ -220,9 +282,28 @@ ThumbnailView.prototype.focus_post = function(post_id)
     shift_pixels_right += document.documentElement.scrollLeft;
 
   padding.setStyle({marginLeft: shift_pixels_right + "px"});
-  thumb.down(".inner").setStyle({overflow: "visible"});
-  thumb.setStyle({zIndex: 30});
 
+  this.centered_post_id = post_id;
+}
+
+ThumbnailView.prototype.expand_post = function(post_id)
+{
+  if(this.expanded_post_id != null)
+  {
+    var old_thumb = $("p" + this.expanded_post_id);
+    old_thumb.removeClassName("expanded");
+  }
+
+  this.expanded_post_id = post_id;
+  if(post_id == null)
+    return;
+
+  var thumb = $("p" + post_id);
+  thumb.addClassName("expanded");
+}
+
+ThumbnailView.prototype.focus_post = function(post_id)
+{
   /* If we're browsing a pool, set the title. */
   if(this.pool)
   {
@@ -243,7 +324,6 @@ ThumbnailView.prototype.focus_post = function(post_id)
 
   this.ignore_hash_changes_until = (new Date()).valueOf() + 100;
   document.location.hash = post_id;
-  this.focused_post_id = post_id;
 }
 
 ThumbnailView.prototype.create_thumb = function(post_id)
@@ -267,9 +347,9 @@ ThumbnailView.prototype.create_thumb = function(post_id)
   var crop_left = (width - visible_width) / 2;
 
   var div =
-    '<div class="inner" style="overflow: hidden; width: ${block_size_x}px; height: ${block_size_y}px;">' + 
+    '<div class="inner" style="width: ${block_size_x}px; height: ${block_size_y}px;">' + 
       '<a class="thumb" href="${target_url}">' +
-        '<img src="${preview_url}" style="margin-left: -${crop_left}px;" alt="" class="${image_class}" width="${width}" height="${height}">'
+        '<img src="about:blank" thumb:src="${preview_url}" style="margin-left: -${crop_left}px;" alt="" class="${image_class}" width="${width}" height="${height}">'
       '</a>' +
     '</div>';
   div = div.subst({
@@ -282,7 +362,7 @@ ThumbnailView.prototype.create_thumb = function(post_id)
     height: height,
     image_class: "preview"
   });
-  var li_class = "";
+  var li_class = "post-thumb";
   li_class += " creator-id-" + post.creator_id;
   if(post.status == "flagged") li_class += " flagged";
   if(post.has_children) li_class += " has-children";
@@ -298,16 +378,35 @@ ThumbnailView.prototype.create_thumb = function(post_id)
   item.setStyle({width: block_size[0] + "px"});
 
   item.down("A.thumb").observe("click", this.thumbnail_click_event);
+  item.down("A.thumb").observe("dblclick", this.thumbnail_dblclick_event);
+
   return item;
 }
 
 ThumbnailView.prototype.thumbnail_click_event = function(event)
 {
-    event.preventDefault();
-    var li = event.target.up("LI");
-    this.focus_post(li.post_id);
-    pool_browser.set_post(li.post_id);
+  event.preventDefault();
+  var li = event.target.up("LI");
+  this.focus_post(li.post_id);
+  this.set_active_post(li.post_id);
 }
+
+ThumbnailView.prototype.thumbnail_dblclick_event = function(event)
+{
+  this.show_thumb_bar(false)
+}
+
+ThumbnailView.prototype.show_thumb_bar = function(shown)
+{
+  this.thumb_container_shown = shown;
+  this.container.show(shown);
+}
+
+ThumbnailView.prototype.toggle_thumb_bar = function()
+{
+  this.show_thumb_bar(!this.thumb_container_shown);
+}
+
 
 /* Return the next or previous post, wrapping around if necessary. */
 ThumbnailView.prototype.get_adjacent_post_id_wrapped = function(post_id, next)

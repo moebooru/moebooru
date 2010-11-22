7c16c7ab1308e093e745af60997af62c782447da
petopeto <none@none>
2010-11-22 01:06:11 +0000

    sort_array_by_distance helper
    --HG--
    branch : moe
    extra : convert_revision : svn%3A2d28d66d-8d94-df11-8c86-00306ef368cb/trunk/moe%40494

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -333,3 +333,27 @@ Ajax.Request.prototype.success = function()
   return this.successBase();
 }
 
+/*
+ * Return the values of list starting at idx and moving outwards.
+ *
+ * sort_array_by_distance([0,1,2,3,4,5,6,7,8,9], 5)
+ * [5,4,6,3,7,2,8,1,9,0]
+ */
+sort_array_by_distance = function(list, idx)
+{
+  var ret = [];
+  ret.push(list[idx]);
+  for(var distance = 1; ; ++distance)
+  {
+    var length = ret.length;
+    if(idx-distance >= 0)
+      ret.push(list[idx-distance]);
+    if(idx+distance < list.length)
+      ret.push(list[idx+distance]);
+    if(length == ret.length)
+      break;
+  }
+
+  return ret;
+}
+

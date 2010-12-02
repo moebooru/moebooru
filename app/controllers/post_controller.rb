318eb9d23f39e2ff8a31ac4c69a6eb9b0f60e705
petopeto <none@none>
2010-12-02 00:27:46 +0000

    return tags with post/batch_update
    --HG--
    branch : moe
    extra : convert_revision : svn%3A2d28d66d-8d94-df11-8c86-00306ef368cb/trunk/moe%40711

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -202,11 +202,12 @@ class PostController < ApplicationController
 
     # Updates to one post may affect others, so only generate the return list after we've already
     # updated everything.
-    ret = Post.find_by_sql(["SELECT * FROM posts WHERE id IN (?)", ids.map { |id, t| id }])
+    posts = Post.find_by_sql(["SELECT * FROM posts WHERE id IN (?)", ids.map { |id, t| id }])
+    api_data = Post.batch_api_data(posts, :include_tags => true)
 
     url = params[:url]
     url = {:action => "index"} if not url
-    respond_to_success("Posts updated", url, :api => {:posts => ret})
+    respond_to_success("Posts updated", url, :api => api_data)
   end
 
   def delete

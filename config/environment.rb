63f1351230cb72e3820411879d896970f1a9cf52
Edho Arief <edho@myconan.net>
2012-03-04 15:02:38 +0000

    No need for actionmailer tls patch anymore.
    --HG--
    extra : rebase_source : 912185d88c3b719bd3c9f1b6d3f5a1ffa9669a52

--- config/environment.rb
+++ config/environment.rb
@@ -27,24 +27,3 @@ Rails::Initializer.run do |config|
   # (enables use of different database adapters for development and test environments)
   config.active_record.schema_format = :sql
 end
-
-if RAILS_GEM_VERSION == '2.2.2'
-  module ActionMailer
-    class Base
-      def perform_delivery_smtp(mail)
-        destinations = mail.destinations
-        mail.ready_to_send
-        sender = mail['return-path'] || mail.from
-
-        smtp = Net::SMTP.new(smtp_settings[:address], smtp_settings[:port])
-        smtp.enable_starttls_auto if smtp_settings[:enable_starttls_auto] && smtp.respond_to?(:enable_starttls_auto)
-        smtp.start(smtp_settings[:domain], smtp_settings[:user_name], smtp_settings[:password],
-              smtp_settings[:authentication]) do |smtp|
-                smtp.sendmail(mail.encoded, sender, destinations)
-              end
-       end
-    end
-  end
-else
-  raise "Remove ActionMailer TLS patch"
-end

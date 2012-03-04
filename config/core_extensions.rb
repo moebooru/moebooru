6a315d5a49a1db17ef6e923fc08471b5d25da608
Edho Arief <edho@myconan.net>
2012-03-04 15:46:55 +0000

    sanitize_sql is now sanitize_sql_array. Still protected method.
    --HG--
    extra : rebase_source : 343621a63f262f193c0063ffbdf76e77bf620c3a

--- config/core_extensions.rb
+++ config/core_extensions.rb
@@ -1,15 +1,15 @@
 class ActiveRecord::Base
   class << self
-    public :sanitize_sql
+    public :sanitize_sql_array
   end
   
   %w(execute select_value select_values select_all).each do |method_name|
     define_method("#{method_name}_sql") do |sql, *params|
-      ActiveRecord::Base.connection.__send__(method_name, self.class.sanitize_sql([sql, *params]))
+      ActiveRecord::Base.connection.__send__(method_name, self.class.sanitize_sql_array([sql, *params]))
     end
 
     self.class.__send__(:define_method, "#{method_name}_sql") do |sql, *params|
-      ActiveRecord::Base.connection.__send__(method_name, ActiveRecord::Base.sanitize_sql([sql, *params]))
+      ActiveRecord::Base.connection.__send__(method_name, ActiveRecord::Base.sanitize_sql_array([sql, *params]))
     end
   end
 end

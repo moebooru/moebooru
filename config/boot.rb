f24ced97a8c4ed99b597e7b34d4974de4d6e31a4
Edho Arief <edho@myconan.net>
2012-03-12 09:40:48 +0000

    Bundler support.
    --HG--
    extra : rebase_source : 77236e80996993a223774c2cffbbc45597ba99bf

--- config/boot.rb
+++ config/boot.rb
@@ -110,5 +110,20 @@ module Rails
   end
 end
 
+# Bundler in 2.3
+class Rails::Boot
+  def run
+    load_initializer
+
+    Rails::Initializer.class_eval do
+      def load_gems
+        @bundler_loaded ||= Bundler.require :default, Rails.env
+      end
+    end
+
+    Rails::Initializer.run(:set_load_path)
+  end
+end
+
 # All that for this:
 Rails.boot!

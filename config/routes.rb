b5934ff586f594a15c6f5843fb98e6cb29da9ed2
Edho Arief <edho@myconan.net>
2012-03-28 14:49:40 +0100

    " => ' (config/routes.rb)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root :controller => "static", :action => "index"
-  map.connect "post/show/:id/:tag_title", :controller => "post", :action => "show", :requirements => {:id => /\d+/}
-  map.connect "pool/zip/:id/:filename", :controller => "pool", :action => "zip", :requirements => {:id => /\d+/, :filename => /.*/}
-  map.connect ":controller/:action/:id.:format", :requirements => {:id => /[-\d]+/}
-  map.connect ":controller/:action/:id", :requirements => {:id => /[-\d]+/}
-  map.connect ":controller/:action.:format"
-  map.connect ":controller/:action"
+  map.root :controller => 'static', :action => 'index'
+  map.connect 'post/show/:id/:tag_title', :controller => 'post', :action => 'show', :requirements => {:id => /\d+/}
+  map.connect 'pool/zip/:id/:filename', :controller => 'pool', :action => 'zip', :requirements => {:id => /\d+/, :filename => /.*/}
+  map.connect ':controller/:action/:id.:format', :requirements => {:id => /[-\d]+/}
+  map.connect ':controller/:action/:id', :requirements => {:id => /[-\d]+/}
+  map.connect ':controller/:action.:format'
+  map.connect ':controller/:action'
 
-  map.connect "histogram", :controller => "post", :action => "histogram"
-  map.connect "download", :controller => "post", :action => "download"
+  map.connect 'histogram', :controller => 'post', :action => 'histogram'
+  map.connect 'download', :controller => 'post', :action => 'download'
 end

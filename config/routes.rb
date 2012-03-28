d2b797ccac5701b48249cc37197b0f7e31d3a528
Edho Arief <edho@myconan.net>
2012-03-28 14:35:41 +0100

    Newer syntax for root route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.connect "", :controller => "static", :action => "index"
+  map.root :controller => "static", :action => "index"
   map.connect "post/show/:id/:tag_title", :controller => "post", :action => "show", :requirements => {:id => /\d+/}
   map.connect "pool/zip/:id/:filename", :controller => "pool", :action => "zip", :requirements => {:id => /\d+/, :filename => /.*/}
   map.connect ":controller/:action/:id.:format", :requirements => {:id => /[-\d]+/}

4343a42ef32264ecf6c0bb4de0c9d1c20d1ae7f7
Edho Arief <edho@myconan.net>
2012-03-28 11:50:09 +0100

    Newer syntax. A bit cleaner.

--- app/daemons/job_task_processor.rb
+++ app/daemons/job_task_processor.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
 ENV['NEWRELIC_ENABLE'] = 'false'
-require File.dirname(__FILE__) + '/../../config/environment'
+require File.expand_path('../../../config/environment', __FILE__)
 
 JobTask.execute_all
--- app/daemons/job_task_processor_ctl.rb
+++ app/daemons/job_task_processor_ctl.rb
@@ -4,4 +4,4 @@ ENV['NEWRELIC_ENABLE'] = 'false'
 require 'rubygems'
 require 'daemons'
 
-Daemons.run(File.dirname(__FILE__) + "/job_task_processor.rb", :log_output => true, :dir => "../../log")
+Daemons.run(File.expand_path("../job_task_processor.rb", __FILE__), :log_output => true, :dir => "../../log")

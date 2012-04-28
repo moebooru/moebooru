9fa513a7da4a52bb0fa47be0af71a441609c0795
Edho Arief <edho@myconan.net>
2012-04-28 11:39:27 -0700

    Welcome to 2.3.0-beta.

--- config/default_config.rb
+++ config/default_config.rb
@@ -1,5 +1,5 @@
-# The version of this Danbooru.
-CONFIG["version"] = "2.3.0-alpha"
+# The version of this Moebooru.
+CONFIG["version"] = "2.3.0-beta"
 
 # The default name to use for anyone who isn't logged in.
 CONFIG["default_guest_name"] = "Anonymous"

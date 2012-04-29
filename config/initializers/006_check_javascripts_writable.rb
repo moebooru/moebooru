9418ad105d5ccba7812c50886a173af4cb9600ce
Edho Arief <edho@myconan.net>
2012-04-29 11:03:28 -0700

    Removed useless check.

--- config/initializers/006_check_javascripts_writable.rb
+++
@@ -1,9 +0,0 @@
-if true
-  path = ""
-  path += "#{Rails.root}/" if defined?(RAILS_ROOT)
-  path += "public/javascripts"
-
-  if not File.writable?(path)
-    raise "Path must be writable: %s" % path
-  end
-end

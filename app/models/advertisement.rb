e7150a5600dce6e8ea3102a26d2712a4ba34dce6
Edho Arief <edho@myconan.net>
2012-04-29 23:03:34 -0700

    Moved advertisement search logic to where it belongs (models).

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,3 +1,7 @@
 class Advertisement < ActiveRecord::Base
   validates_inclusion_of :ad_type, :in => %w(horizontal vertical)
+
+  def self.random(type = 'vertical')
+    self.find(:first, :conditions => { :ad_type => type, :status => 'active' }, :order => 'random()')
+  end
 end

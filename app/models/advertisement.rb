1b7a19da71494cf730e094d5e0a8f4c9c2339015
Edho Arief <edho@myconan.net>
2012-05-01 09:40:53 +0000

    Allows reset of hit count.

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -4,4 +4,14 @@ class Advertisement < ActiveRecord::Base
   def self.random(type = 'vertical')
     self.find(:first, :conditions => { :ad_type => type, :status => 'active' }, :order => 'random()')
   end
+
+  # virtual method for resetting hit count in view
+  def reset_hit_count=(is_reset)
+    self.hit_count = 0 if is_reset == '1'
+  end
+
+  # virtual method for no-reset default in view's form
+  def reset_hit_count
+    '0'
+  end
 end

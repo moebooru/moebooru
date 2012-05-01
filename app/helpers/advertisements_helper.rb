0e62d5d1d7e06745974f579b03398993f2d79393
Edho Arief <edho@myconan.net>
2012-05-01 12:44:07 +0000

    convert tab to (2) space.

--- app/helpers/advertisements_helper.rb
+++ app/helpers/advertisements_helper.rb
@@ -1,10 +1,10 @@
 module AdvertisementsHelper
   def print_advertisement(ad_type)
-		if CONFIG["can_see_ads"].call(@current_user)
-	    ad = Advertisement.random(ad_type)
-	    content_tag("div", link_to(image_tag(ad.image_url, :alt => "Advertisement", :width => ad.width, :height => ad.height), redirect_advertisement_path(ad)), :style => "margin-bottom: 1em;")
-		else
-			""
-		end
+    if CONFIG["can_see_ads"].call(@current_user)
+      ad = Advertisement.random(ad_type)
+      content_tag("div", link_to(image_tag(ad.image_url, :alt => "Advertisement", :width => ad.width, :height => ad.height), redirect_advertisement_path(ad)), :style => "margin-bottom: 1em;")
+    else
+      ""
+    end
   end
 end

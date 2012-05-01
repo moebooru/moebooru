d556576a0d6b7fe2fb25000ccc7f937ef6fb502d
Edho Arief <edho@myconan.net>
2012-05-01 12:44:29 +0000

    Removed unused else.

--- app/helpers/advertisements_helper.rb
+++ app/helpers/advertisements_helper.rb
@@ -3,8 +3,6 @@ module AdvertisementsHelper
     if CONFIG["can_see_ads"].call(@current_user)
       ad = Advertisement.random(ad_type)
       content_tag("div", link_to(image_tag(ad.image_url, :alt => "Advertisement", :width => ad.width, :height => ad.height), redirect_advertisement_path(ad)), :style => "margin-bottom: 1em;")
-    else
-      ""
     end
   end
 end

3d55ebd860387a0b1de74290f8c99bf27d379c36
Edho Arief <edho@myconan.net>
2012-05-05 13:01:50 +0000

    Add unique index to posts_tags to boost join performace.

---
+++ db/migrate/20120505130017_add_unique_index_to_posts_tag.rb
@@ -0,0 +1,9 @@
+class AddUniqueIndexToPostsTag < ActiveRecord::Migration
+  def self.up
+    add_index :posts_tags, [:post_id, :tag_id], :unique => true
+  end
+
+  def self.down
+    remove_index :posts_tags, :column => [:post_id, :tag_id]
+  end
+end

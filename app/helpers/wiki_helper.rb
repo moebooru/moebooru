d922eb47e626de295ea07ce03219b9627ad05165
Edho Arief <edho@myconan.net>
2012-05-06 10:35:06 +0000

    Infinitely better wiki history page.

--- app/helpers/wiki_helper.rb
+++ app/helpers/wiki_helper.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 module WikiHelper
   def linked_from(to)
     links = to.find_pages_that_link_to_this.map do |page|
@@ -5,5 +6,26 @@ module WikiHelper
     end.join(", ")
 
     links.empty? ? "None" : links
+  end
+
+  # Generates content for "Changes" column on history page.
+  # Not filtered, must return HTML-safe string.
+  def page_change(w)
+    if w.what_updated.empty?
+      'No change'
+    else
+      w.what_updated.map do |c|
+        case c
+        when :initial
+          'first version'
+        when :body
+          'content update'
+        when :title
+          "page rename (#{h w.title} ← #{h w.prev.title})"
+        when :is_locked
+          w.is_locked ? 'page lock' : 'page unlock'
+        end
+      end.join(', ').capitalize
+    end
   end
 end
--- app/models/wiki_page_version.rb
+++ app/models/wiki_page_version.rb
@@ -3,6 +3,22 @@ class WikiPageVersion < ActiveRecord::Base
     return User.find_name(self.user_id)
   end
 
+  def what_updated
+    what = []
+    if self.version > 1
+      what += [:body] if self.body != self.prev.body
+      what += [:title] if self.title != self.prev.title
+      what += [:is_locked] if self.is_locked != self.prev.is_locked
+    else
+      what += [:initial]
+    end
+    return what
+  end
+
+  def prev
+    WikiPageVersion.first(:conditions => { :wiki_page_id => self.wiki_page_id, :version => self.version - 1 })
+  end
+
   def pretty_title
     self.title.tr("_", " ")
   end

fc35667c103cdb3a9c1985b30030dd084045ccde
Edho Arief <edho@myconan.net>
2012-05-05 22:11:25 -0700

    Proper touch method.

--- app/models/pool_post.rb
+++ app/models/pool_post.rb
@@ -1,7 +1,7 @@
 class PoolPost < ActiveRecord::Base
   set_table_name "pools_posts"
   belongs_to :post
-  belongs_to :pool
+  belongs_to :pool, :touch => true
   belongs_to :next_post, :class_name => "Post", :foreign_key => "next_post_id"
   belongs_to :prev_post, :class_name => "Post", :foreign_key => "prev_post_id"
   versioned_parent :pool

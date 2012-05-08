5e64e8aa4f46aa2990d9f60fbdb252e136209948
Edho Arief <edho@myconan.net>
2012-05-08 07:06:32 +0000

    Reinjected ancient method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -314,7 +314,31 @@ module ApplicationHelper
   include ActionView::Helpers::UrlHelper
 
 private
-  alias_method :orig_convert_options_to_javascript!, :convert_options_to_javascript!
+  # Imported from Rails 2.3
+  # FIXME: this is private method and should be replaced
+  #        with something better.
+  def orig_convert_options_to_javascript!(html_options, url = '')
+    confirm, popup = html_options.delete("confirm"), html_options.delete("popup")
+
+    method, href = html_options.delete("method"), html_options['href']
+
+    html_options["onclick"] = case
+      when popup && method
+        raise ActionView::ActionViewError, "You can't use :popup and :method in the same link"
+      when confirm && popup
+        "if (#{confirm_javascript_function(confirm)}) { #{popup_javascript_function(popup)} };return false;"
+      when confirm && method
+        "if (#{confirm_javascript_function(confirm)}) { #{method_javascript_function(method, url, href)} };return false;"
+      when confirm
+        "return #{confirm_javascript_function(confirm)};"
+      when method
+        "#{method_javascript_function(method, url, href)}return false;"
+      when popup
+        "#{popup_javascript_function(popup)}return false;"
+      else
+        html_options["onclick"]
+    end
+  end
   # This handles link_to (and others, not tested).
   def convert_options_to_javascript!(html_options, url = '')
     level = html_options["level"]

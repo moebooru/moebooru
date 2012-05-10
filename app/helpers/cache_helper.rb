55b58bc52a9bc8241b3ab18a2a18396f83ad39ca
Edho Arief <edho@myconan.net>
2012-05-10 12:37:11 -0700

    Fixed cache key building mainly by removing the else part which didn't make sense.

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -2,7 +2,7 @@ module CacheHelper
   def build_cache_key(base, tags, page, limit, options = {})
     page = page.to_i
     page = 1 if page < 1
-    tags = tags.to_s.downcase.scan(/\S+/).sort
+    tags = tags.to_s.downcase.split.sort
 
     if options[:user]
       user_level = options[:user].level
@@ -11,14 +11,8 @@ module CacheHelper
       user_level = "?"
     end
 
-    if tags.empty? || tags.any? {|x| x =~ /(?:^-|[*:])/}
-      version = Rails.cache.read("$cache_version").to_i
-      tags = tags.join(",")
-    else
-      version = "?"
-      tags = tags.map {|x| x + ":" + Rails.cache.read("tag:#{x}").to_i.to_s}.join(",")
-    end
-    tags = Base64.urlsafe_encode64(tags)
+    version = Rails.cache.read("$cache_version").to_i
+    tags = Base64.urlsafe_encode64(tags.join(" "))
 
     ["#{base}/v=#{version}&t=#{tags}&p=#{page}&ul=#{user_level}&l=#{limit}", 0]
   end

28a8769c1d7c32fdfa46a3a2bbfcb1ea54b2038c
Edho Arief <edho@myconan.net>
2012-05-12 03:56:55 +0000

    Fix for incorrect error response parameter.

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -130,7 +130,7 @@ class WikiController < ApplicationController
       if @page.save
         respond_to_success("Page reverted", :action => "show", :title => params[:title])
       else
-        respond_to_error(@page)
+        respond_to_error("Error reverting page", { :action => 'show', :title => params[:title] })
       end
     end
   end

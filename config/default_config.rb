153863fa03d2769d699a37b59e8b709a83021659
Edho Arief <edho@myconan.net>
2012-05-20 17:43:47 -0700

    Sufficiently advanced. Entering beta-5.

--- config/default_config.rb
+++ config/default_config.rb
@@ -1,5 +1,5 @@
 # The version of this Moebooru.
-CONFIG["version"] = "2.3.0-beta.4"
+CONFIG["version"] = "2.3.0-beta.5"
 
 # The default name to use for anyone who isn't logged in.
 CONFIG["default_guest_name"] = "Anonymous"

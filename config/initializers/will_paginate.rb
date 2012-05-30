88d32bfdfe64c4bb9627376469ee6fe1e116af3a
Edho Arief <edho@myconan.net>
2012-05-30 23:01:24 +0700

    Forgot to escape the page navigation links characters.

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,2 +1,2 @@
-WillPaginate::ViewHelpers.pagination_options[:next_label] = '>>'
-WillPaginate::ViewHelpers.pagination_options[:previous_label] = '<<'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = ERB::Util.html_escape '>>'
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = ERB::Util.html_escape '<<'

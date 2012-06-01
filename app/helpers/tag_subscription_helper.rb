8009f0b12f862af00bfa19ee0ce98c3563cd74a0
Edho Arief <edho@myconan.net>
2012-06-01 12:54:46 -0700

    html_safe~, and removed h(...)

--- app/helpers/tag_subscription_helper.rb
+++ app/helpers/tag_subscription_helper.rb
@@ -2,14 +2,14 @@ module TagSubscriptionHelper
   def tag_subscription_listing(user)
     html = []
     user.tag_subscriptions.each do |tag_subscription|
-      html << '<span class="group"><strong>' + link_to(h(tag_subscription.name), :controller => "post", :action => "index", :tags => "sub:#{user.name}:#{tag_subscription.name}") + '</strong>: '
+      html << %{<span class="group"><strong>#{link_to(tag_subscription.name, :controller => "post", :action => "index", :tags => "sub:#{user.name}:#{tag_subscription.name}")}</strong>: }
       tags = tag_subscription.tag_query.scan(/\S+/).sort
       group = []
       tags.each do |tag|
-        group << link_to(h(tag), :controller => "post", :action => "index", :tags => tag)
+        group << link_to(tag, :controller => "post", :action => "index", :tags => tag)
       end
-      html[-1] << group.join(" ") + '</span>'
+      html[-1] << %{#{group.join(' ')}</span>}
     end
-    html.join(" ")
+    html.join(' ').html_safe
   end
 end

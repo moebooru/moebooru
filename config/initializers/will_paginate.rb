95a57077b008c0cdc35b05d0548b015e3771d0e5
Edho Arief <edho@myconan.net>
2012-06-01 18:17:56 +0700

    Proper location for will_paginate label setting.

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,4 +1,2 @@
-WillPaginate::ViewHelpers.pagination_options[:next_label] = ERB::Util.html_escape '>>'
-WillPaginate::ViewHelpers.pagination_options[:previous_label] = ERB::Util.html_escape '<<'
 WillPaginate::ViewHelpers.pagination_options[:inner_window] = 2
 WillPaginate::ViewHelpers.pagination_options[:outer_window] = 1

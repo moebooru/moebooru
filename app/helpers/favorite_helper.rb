5e1c7dde429bf297218103d27aaf55a794b43513
Edho Arief <edho@myconan.net>
2012-06-01 09:44:57 -0700

    A bit cleaner(?) helper.

--- app/helpers/favorite_helper.rb
+++ app/helpers/favorite_helper.rb
@@ -1,20 +1,21 @@
 module FavoriteHelper
   def favorite_list(post)
-    html = ""
+    html = ''.html_safe
     
     users = post.favorited_by
+    users_link = users.map { |user| link_to user.pretty_name, { :controller => :user, :action => :show, :id => user.id } }
 
     if users.empty?
-      html << "no one"
+      html << 'no one'
     else
-      html << users.slice(0, 6).map {|user| link_to(user.pretty_name, :controller => "user", :action => "show", :id => user.id)}.join(", ")
+      html << users_link.first(6).join(', ').html_safe
 
       if users.size > 6
-        html << content_tag("span", :id => "remaining-favs", :style => "display: none;") do
-          ", " + users.slice(6..-1).map {|user| link_to(user.pretty_name, {:controller => "user", :action => "show", :id => user.id})}.join(", ").html_safe
+        html << content_tag(:span, :id => 'remaining-favs', :style => 'display: none;') do
+          ", #{users_link.slice(6..-1).join(', ')}".html_safe
         end
-        html << content_tag("span", :id => "remaining-favs-link") do
-          " (" + link_to_function("#{users.size - 6} more", "$('remaining-favs').show(); $('remaining-favs-link').hide()") + ")".html_safe
+        html << content_tag(:span, :id => 'remaining-favs-link') do
+          " (#{link_to_function "#{users.size - 6} more", "$('remaining-favs').show(); $('remaining-favs-link').hide()"})".html_safe
         end
       end
     end

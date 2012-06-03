01f1096688c44ead1de9f7e8913593a6a35f29f9
Edho Arief <edho@myconan.net>
2012-06-03 07:46:16 -0700

    Much simpler calculate_related_by_type.
    --HG--
    extra : rebase_source : 88816a1085fcefcbe39edb49b22b174ac912c931

--- app/models/tag/related_tag_methods.rb
+++ app/models/tag/related_tag_methods.rb
@@ -1,42 +1,11 @@
 module TagRelatedTagMethods
   module ClassMethods
     def calculate_related_by_type(tag, type, limit = 25)
-      if CONFIG["enable_caching"] && tag.size < 230
-        results = Rails.cache.read("reltagsbytype/#{type}/#{Tag.cache_key_enc(tag)}")
-        
-        if results
-          return JSON.parse(results)
+      Rails.cache.fetch({ :category => :reltags_by_type, :type => type, :tag => tag }, :expires_in => 1.hour) do
+        Tag.joins(:posts).where(:posts => { :id => Post.has_tag(tag) }, :tag_type => type).group(:name).count(:all, :order => 'count_all DESC', :limit => limit).reduce([]) do
+          |result, hash| result << { 'name' => hash[0], 'post_count' => hash[1] }
         end
       end
-      
-      sql = <<-EOS
-        SELECT (SELECT name FROM tags WHERE id = pt0.tag_id) AS name,
-        COUNT(pt0.tag_id) AS post_count
-        FROM posts_tags pt0, posts_tags pt1
-        WHERE pt0.post_id = pt1.post_id
-        AND (SELECT TRUE FROM POSTS p0 WHERE p0.id = pt0.post_id AND p0.status <> 'deleted')
-        AND pt1.tag_id = (SELECT id FROM tags WHERE name = ?)
-        AND pt0.tag_id IN (SELECT id FROM tags WHERE tag_type = ?)
-        GROUP BY pt0.tag_id
-        ORDER BY post_count DESC
-        LIMIT ?
-      EOS
-
-      begin
-        results = select_all_sql(sql, tag, type, limit)
-      rescue Exception
-        results = []
-      end
-      
-      if CONFIG["enable_caching"] && tag.size < 230
-        post_count = (Tag.find_by_name(tag).post_count rescue 0) / 3
-        post_count = 12 if post_count < 12
-        post_count = 200 if post_count > 200
-        
-        Rails.cache.write("reltagsbytype/#{type}/#{Tag.cache_key_enc(tag)}", results.map {|x| {"name" => x["name"], "post_count" => x["post_count"]}}.to_json, :expires_in => post_count.hours)
-      end
-      
-      return results
     end
 
     def calculate_related(tags)

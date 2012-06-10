add8deda68e17202d68485e84f83c303e2e5a5fe
Edho Arief <edho@myconan.net>
2012-06-10 04:51:34 -0700

    Moved string core_ext to its own file.

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -18,4 +18,20 @@ class String
       return self.force_encoding('UTF-8').encode('UTF-16', :invalid => :replace, :replace => '').encode('UTF-8')
     end
   end
+
+  # Escapes string to be usable in a SQL LIKE.
+  # Adds backslash to \, %, and _ and replace * with % (SQL wildcard)
+  def to_escaped_for_sql_like
+    return self.gsub(/[\\%_]/) { |x| '\\' + x }.gsub('*', '%')
+  end
+
+  # The characters \()&|!: must be escaped
+  # by prepending them with \ before passed to tsquery.
+  def to_escaped_for_tsquery
+    return self.gsub(/[\\()&|!:]/) { |x| '\\' + x }
+  end
+
+  def to_escaped_js
+    return self.gsub(/\\/, '\0\0').gsub(/['"]/) {|m| "\\#{m}"}.gsub(/\r\n|\r|\n/, '\\n')
+  end
 end

84caf5bce2122ac702699b8e20d6d636fdad9132
Edho Arief <edho@myconan.net>
2012-06-09 23:24:09 -0700

    iqdb has no problem returning invalid utf-8 it seems.

--- lib/similar_images.rb
+++ lib/similar_images.rb
@@ -1,5 +1,6 @@
 require 'multipart'
 require 'external_post'
+require 'iconv'
 
 module SimilarImages
   def get_services(services)
@@ -99,7 +100,14 @@ module SimilarImages
     similarity = {}
     preview_url = ""
     next_id = 1
+    # The xml input obtained can't be trusted to contain only
+    # proper utf-8 characters. Strip invalid utf-8 using iconv.
+    ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
     server_responses.map do |server, xml|
+      # The weird iconv parameter is required thanks to iconv not stripping
+      # invalid character if it's at the end of input string.
+      # Reference: http://po-ru.com/diary/fixing-invalid-utf-8-in-ruby-revisited
+      xml = ic.iconv(xml + ' ')[0..-2]
       doc = begin
         Nokogiri::XML xml
       rescue Exception => e

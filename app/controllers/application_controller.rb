50f86986a1068945a8cd02799cbe6de8b5129cc1
dovac <dobacco@gmail.com>
2012-06-10 12:33:24 -0700

    set lang via cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
  
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+  if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
+    cookies['locale'] = { :value => params[:locale], :expires => 1.year.from_now }
+    I18n.locale = params[:locale].to_sym
+  elsif cookies['locale'] && I18n.available_locales.include?(cookies['locale'].to_sym)
+    I18n.locale = cookies['locale'].to_sym
+  end
   end
 
   # This is a proxy class to make various nil checks unnecessary

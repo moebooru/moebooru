2c3158c8826b2934f55808423e44558a969b23c5
Edho Arief <edho@myconan.net>
2012-06-17 12:55:39 -0700

    Use time for cache_version instead of random (incrementing) integer.
    Fixes post count.
    
    --HG--
    branch : 2.3-stable

--- lib/core_ext/active_support_cache_store_ext.rb
+++ lib/core_ext/active_support_cache_store_ext.rb
@@ -1,12 +1,9 @@
 module ActiveSupport::Cache
   class Store
     def expire(options = {})
-        tags = options[:tags]
-        cache_version = read("$cache_version").to_i
-  
-        write("$cache_version", cache_version + 1)
+      write('$cache_version', Time.now.to_i)
     end
-    
+
     def expire_tag_version
       # $tag_version is bumped when the type of a tag is changed in Tags, if
       # a new tag is created, or if a tag's post_count becomes nonzero.

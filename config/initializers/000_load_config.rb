70c89a756805e51a5c4b6fe221520600fbee391e
Edho Arief <edho@myconan.net>
2012-06-23 00:21:28 +0700

    Explicit require for most things in lib/ as part of rails 3 change.

--- config/initializers/000_load_config.rb
+++ config/initializers/000_load_config.rb
@@ -4,10 +4,8 @@ CONFIG["url_base"] ||= "http://" + CONFIG["server_host"]
   CONFIG[key] = ServerKey[key] if ServerKey[key]
 end
 
-require 'post_save'
 require 'base64'
 require 'diff/lcs/array'
-require 'image_size'
 require 'ipaddr'
 require 'open-uri'
 require 'socket'
@@ -17,9 +15,27 @@ require 'net/http'
 require 'aws/s3' if [:amazon_s3, :local_flat_with_amazon_s3_backup].include?(CONFIG["image_store"])
 require 'danbooru_image_resizer/danbooru_image_resizer'
 require 'google_chart' if CONFIG["enable_reporting"]
-require 'core_extensions'
 require 'download'
 require 'sys/cpu' if CONFIG["load_average_threshold"]
 require 'fileutils'
-require 'versioning'
+
+# lib requires
+require 'core_extensions'
+require 'diff'
+require 'download'
 require 'dtext'
+require 'external_post'
+require 'extract_urls'
+require 'image_size'
+# This one is already loaded by default_config.rb (and it is the only user)
+#require 'languages'
+require 'mirror'
+require 'multipart'
+require 'nagato'
+require 'post_save'
+require 'query_parser'
+require 'report'
+require 'similar_images'
+require 'tokenize'
+require 'translate'
+require 'versioning'

0bf3de923fc426752302e7343af44e637e309e36
Edho Arief <edho@myconan.net>
2012-06-24 01:39:28 -0700

    Ignores post/show's format (always html).

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,7 @@ Moebooru::Application.routes.draw do
     match 'favorite/list_users(.:format)' => 'favorite#list_users', :constraints => { :format => 'json' }
   end
 
-  match 'post/show/:id/:tag_title' => 'post#show', :constraints => { :id => /\d+/ }
+  match 'post/show/:id/*tag_title' => 'post#show', :constraints => { :id => /\d+/ }, :format => false
   match 'pool/zip/:id/:filename' => 'pool#zip', :constraints => { :id => /\d+/, :filename => /.*/ }
   match ':controller(/:action(/:id))', :id => /\d+/
   match ':controller/:action.:format' => '#index'

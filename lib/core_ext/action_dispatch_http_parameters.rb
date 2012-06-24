b0a5743f26cf4b74373ef0a1eb55a274bbe2514c
Edho Arief <edho@myconan.net>
2012-06-24 06:40:50 -0700

    Fixed the encoding problem once and for all(?)
    ...by importing fix for Rails 4(!)
    
    --HG--
    extra : rebase_source : fd6cdba97f0fcdf166df6ced5477b8af0570a86e

---
+++ lib/core_ext/action_dispatch_http_parameters.rb
@@ -0,0 +1,29 @@
+# This one handles sanitizing params.
+# Taken from https://github.com/rails/rails/pull/3789/files
+require 'core_ext/action_dispatch_encoder'
+module ActionDispatch
+  module Http
+    module Parameters
+    private
+      def encode_params(params)
+        return params unless "ruby".encoding_aware?
+        if params.is_a?(String)
+          return ActionDispatch::Encoder::encode_to_internal(params)
+        elsif !params.is_a?(Hash)
+          return params
+        end
+
+        params.each do |k, v|
+          case v
+          when Hash
+            encode_params(v)
+          when Array
+            v.map! {|el| encode_params(el) }
+          else
+            encode_params(v)
+          end
+        end
+      end
+    end
+  end
+end

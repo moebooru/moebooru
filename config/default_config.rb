e83f3fa7cf8564f47b1052b7ca2c321d5128cacb
Edho Arief <edho@myconan.net>
2012-06-25 20:00:44 +0700

    Version UP

--- config/default_config.rb
+++ config/default_config.rb
@@ -1,6 +1,6 @@
 require File.expand_path '../../lib/languages', __FILE__
 # The version of this Moebooru.
-CONFIG["version"] = "3.0.0-beta.2"
+CONFIG["version"] = "3.2.0-alpha"
 
 # The default name to use for anyone who isn't logged in.
 CONFIG["default_guest_name"] = "Anonymous"

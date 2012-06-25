58230aa0e96e3caa93fe9b556e366147b1afe8e6
Edho Arief <edho@myconan.net>
2012-06-25 02:40:57 -0700

    Threadsafe only for production environment.

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,6 @@ module Moebooru
 
     config.action_mailer.smtp_settings = { :openssl_verify_mode => 'none', :domain => CONFIG['server_host'] }
     config.filter_parameters += [:password]
-    config.threadsafe!
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]

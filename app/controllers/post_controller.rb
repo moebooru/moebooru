ab7fac6e0fcd77a4645b3ace67d080dc928e4c72
Edho Arief <edho@myconan.net>
2012-07-01 16:22:44 +0700

    WIP: force format html for /post/atom.

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -393,11 +393,7 @@ class PostController < ApplicationController
   end
 
   def atom
-    # We get a lot of bogus "/post/atom.feed" requests that spam our error logs.  Make sure
-    # we only try to format atom.xml.
-    if not params[:format].nil? then
-      params[:format] = "html"
-    end
+    params[:format] = "html"
 
     @posts = Post.find_by_sql(Post.generate_sql(params[:tags], :limit => 20, :order => "p.id DESC"))
     headers["Content-Type"] = "application/atom+xml"

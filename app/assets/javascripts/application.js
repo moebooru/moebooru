95637dff69bb5a6c895c9cc4061ab3612922008e
Edho Arief <edho@myconan.net>
2012-07-03 18:31:09 +0700

    Moved javascripts to app/assets.
    --HG--
    rename : public/javascripts/application.js => app/assets/javascripts/application.js
    rename : public/javascripts/browser-img-pool.js => app/assets/javascripts/browser-img-pool.js
    rename : public/javascripts/browser-post-loading.js => app/assets/javascripts/browser-post-loading.js
    rename : public/javascripts/browser-thumb-view.js => app/assets/javascripts/browser-thumb-view.js
    rename : public/javascripts/browser-view.js => app/assets/javascripts/browser-view.js
    rename : public/javascripts/comment.js => app/assets/javascripts/comment.js
    rename : public/javascripts/common.js => app/assets/javascripts/common.js
    rename : public/javascripts/cookie.js => app/assets/javascripts/cookie.js
    rename : public/javascripts/cropper.js => app/assets/javascripts/cropper.js
    rename : public/javascripts/debug-window.js => app/assets/javascripts/debug-window.js
    rename : public/javascripts/dmail.js => app/assets/javascripts/dmail.js
    rename : public/javascripts/favorite.js => app/assets/javascripts/favorite.js
    rename : public/javascripts/forum.js => app/assets/javascripts/forum.js
    rename : public/javascripts/history.js => app/assets/javascripts/history.js
    rename : public/javascripts/inline_image.js => app/assets/javascripts/inline_image.js
    rename : public/javascripts/main_menu.js => app/assets/javascripts/main_menu.js
    rename : public/javascripts/notes.js => app/assets/javascripts/notes.js
    rename : public/javascripts/pool.js => app/assets/javascripts/pool.js
    rename : public/javascripts/post-frame-editor.js => app/assets/javascripts/post-frame-editor.js
    rename : public/javascripts/post-upload-form.js => app/assets/javascripts/post-upload-form.js
    rename : public/javascripts/post.js => app/assets/javascripts/post.js
    rename : public/javascripts/post_mode_menu.js => app/assets/javascripts/post_mode_menu.js
    rename : public/javascripts/post_tag_history.js => app/assets/javascripts/post_tag_history.js
    rename : public/javascripts/preload.js => app/assets/javascripts/preload.js
    rename : public/javascripts/referral.js => app/assets/javascripts/referral.js
    rename : public/javascripts/related_tags.js => app/assets/javascripts/related_tags.js
    rename : public/javascripts/similar-thumbnailing.js => app/assets/javascripts/similar-thumbnailing.js
    rename : public/javascripts/tag_completion.js => app/assets/javascripts/tag_completion.js
    rename : public/javascripts/touchscreen-web-app-helpers.js => app/assets/javascripts/touchscreen-web-app-helpers.js
    rename : public/javascripts/url-hash.js => app/assets/javascripts/url-hash.js
    rename : public/javascripts/user.js => app/assets/javascripts/user.js
    rename : public/javascripts/user_record.js => app/assets/javascripts/user_record.js
    rename : public/javascripts/vote-widget.js => app/assets/javascripts/vote-widget.js
    rename : public/javascripts/builder.js => lib/assets/javascripts/builder.js
    rename : public/javascripts/controls.js => lib/assets/javascripts/controls.js
    rename : public/javascripts/dragdrop.js => lib/assets/javascripts/dragdrop.js
    rename : public/javascripts/effects.js => lib/assets/javascripts/effects.js
    rename : public/javascripts/prototype.js => lib/assets/javascripts/prototype.js
    rename : public/javascripts/rails.js => lib/assets/javascripts/rails.js
    rename : public/javascripts/scriptaculous.js => lib/assets/javascripts/scriptaculous.js
    rename : public/javascripts/slider.js => lib/assets/javascripts/slider.js
    rename : public/javascripts/sound.js => lib/assets/javascripts/sound.js
    rename : public/javascripts/unittest.js => lib/assets/javascripts/unittest.js

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,12 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+//= require prototype
+//= require scriptaculous
+//= require builder
+//= require controls
+//= require dragdrop
+//= require effects
+//= require slider
+//= require sound
+//= require rails
+//= require_tree .

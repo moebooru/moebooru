754a47ea74d81d38b64927060ceb1eb1e7b60a32
Edho Arief <edho@myconan.net>
2012-07-04 21:24:33 -0700

    Explicit i18n locale setting.

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,6 @@ module Moebooru
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = CONFIG['default_locale']
   end
 end
--- config/default_config.rb
+++ config/default_config.rb
@@ -1,3 +1,6 @@
 # If set, email_from is the address the site sends emails as.  If left alone, emails
 # are sent from CONFIG["admin_contact"].
 CONFIG['email_from'] ||= CONFIG['admin_contact']
+
+# Set default local
+CONFIG['default_locale'] ||= 'en'

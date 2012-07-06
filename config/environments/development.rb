3316a534c77908ff5080f5670dd6833d5b947d4f
Edho Arief <edho@myconan.net>
2012-07-06 07:24:49 -0700

    Apparently jruby's pg adapter doesn't support explain.
    --HG--
    extra : rebase_source : f6dc109b15e7d087ed401fa6c9db5d461ab52b75

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Moebooru::Application.configure do
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.active_record.auto_explain_threshold_in_seconds = 0.5 unless RUBY_PLATFORM == 'java'
 
   # Do not compress assets
   config.assets.compress = false

59aaa62610a0f11ce00374eb1c4b945a7868d536
Edho Arief <edho@myconan.net>
2012-07-19 12:45:05 +0200

    As it turned out, the problem is on entirely different place.
    --HG--
    branch : 3.2.0-release

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -75,11 +75,6 @@ class NoteController < ApplicationController
       return
     end
 
-    # The received parameters are float. Force casting to integer.
-    params[:note][:x] = params[:note][:x].to_i
-    params[:note][:y] = params[:note][:y].to_i
-    params[:note][:height] = params[:note][:height].to_i
-    params[:note][:width] = params[:note][:width].to_i
     note.attributes = params[:note]
     note.user_id = @current_user.id
     note.ip_addr = request.remote_ip

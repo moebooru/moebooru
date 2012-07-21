1820735e6c2d5b16bd5405c0d41f18ff42204ac5
Edho Arief <edho@myconan.net>
2012-07-21 17:15:34 +0200

    Script cleanup.

--- script/daemons/job_task_processor_ctl.rb
+++ script/daemons/job_task_processor_ctl.rb
@@ -1,7 +1,4 @@
 #!/usr/bin/env ruby
 
 ENV['NEWRELIC_ENABLE'] = 'false'
-require 'rubygems'
-require 'daemons'
-
-Daemons.run(File.expand_path("../job_task_processor.rb", __FILE__), :log_output => true, :dir => "../../log")
+Daemons.run(File.expand_path('../job_task_processor.rb', __FILE__), :log_output => true, :dir => File.expand_path('../../../log', __FILE__))

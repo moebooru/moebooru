0e2b3c9679d8e7afb00edb9f7813069469774ff3
Edho Arief <edho@myconan.net>
2012-07-21 17:13:26 +0200

    Removed irrelevant scripts.

--- script/build_application_js.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-JS=public/javascripts
-
-echo "" > $JS/application.js
-
-for i in prototype effects controls common cookie comment favorite forum notes pool post post_mode_menu related_tags dmail user_record ; do
-    cat $JS/$i.js >> $JS/application.js
-done

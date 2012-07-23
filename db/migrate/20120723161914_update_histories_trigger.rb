389f3039c6660ef654491ae10839f811b494dade
Edho Arief <edho@myconan.net>
2012-07-23 18:25:40 +0200

    Update ze trigger.

---
+++ db/migrate/20120723161914_update_histories_trigger.rb
@@ -0,0 +1,73 @@
+class UpdateHistoriesTrigger < ActiveRecord::Migration
+  def up
+    execute <<-SQL.strip_heredoc
+      CREATE OR REPLACE FUNCTION history_changes_index_trigger() RETURNS trigger
+          LANGUAGE plpgsql
+          AS $$
+          DECLARE
+            old_tags varchar;
+            old_tags_array varchar[];
+            new_tags_array varchar[];
+            changed_tags_array varchar[];
+            indexed_value varchar;
+          BEGIN
+            IF (new.table_name, new.column_name) IN (('posts', 'cached_tags')) THEN
+              old_tags := prev.value FROM history_changes prev WHERE (prev.id = new.previous_id) LIMIT 1;
+              old_tags_array := regexp_split_to_array(COALESCE(old_tags, ''), ' ');
+              new_tags_array := regexp_split_to_array(COALESCE(new.value, ''), ' ');
+
+              changed_tags_array := get_new_tags(old_tags_array, new_tags_array);
+              changed_tags_array := array_cat(changed_tags_array, get_new_tags(new_tags_array, old_tags_array));
+              indexed_value := join_string(changed_tags_array, ' ');
+            ELSEIF (new.table_name, new.column_name) IN (('pools', 'name')) THEN
+              indexed_value := translate(new.value, '_', ' ');
+            ELSEIF (new.table_name, new.column_name) IN (('posts', 'cached_tags'), ('posts', 'source'), ('pools', 'description'), ('notes', 'body')) THEN
+              indexed_value := new.value;
+            ELSE
+              RETURN new;
+            END IF;
+
+            new.value_index := to_tsvector('public.danbooru', indexed_value);
+
+            RETURN new;
+          END
+          $$;
+    SQL
+  end
+
+  def down
+    execute <<-SQL.strip_heredoc
+      CREATE OR REPLACE FUNCTION history_changes_index_trigger() RETURNS trigger
+          LANGUAGE plpgsql
+          AS $$
+          DECLARE
+            old_tags varchar;
+            old_tags_array varchar[];
+            new_tags_array varchar[];
+            changed_tags_array varchar[];
+            indexed_value varchar;
+          BEGIN
+            IF (new.table_name, new.field) IN (('posts', 'cached_tags')) THEN
+              old_tags := prev.value FROM history_changes prev WHERE (prev.id = new.previous_id) LIMIT 1;
+              old_tags_array := regexp_split_to_array(COALESCE(old_tags, ''), ' ');
+              new_tags_array := regexp_split_to_array(COALESCE(new.value, ''), ' ');
+
+              changed_tags_array := get_new_tags(old_tags_array, new_tags_array);
+              changed_tags_array := array_cat(changed_tags_array, get_new_tags(new_tags_array, old_tags_array));
+              indexed_value := join_string(changed_tags_array, ' ');
+            ELSEIF (new.table_name, new.field) IN (('pools', 'name')) THEN
+              indexed_value := translate(new.value, '_', ' ');
+            ELSEIF (new.table_name, new.field) IN (('posts', 'cached_tags'), ('posts', 'source'), ('pools', 'description'), ('notes', 'body')) THEN
+              indexed_value := new.value;
+            ELSE
+              RETURN new;
+            END IF;
+
+            new.value_index := to_tsvector('public.danbooru', indexed_value);
+
+            RETURN new;
+          END
+          $$;
+    SQL
+  end
+end
--- db/structure.sql
+++ db/structure.sql
@@ -85,7 +85,7 @@ CREATE FUNCTION history_changes_index_trigger() RETURNS trigger
       changed_tags_array varchar[];
       indexed_value varchar;
     BEGIN
-      IF (new.table_name, new.field) IN (('posts', 'cached_tags')) THEN
+      IF (new.table_name, new.column_name) IN (('posts', 'cached_tags')) THEN
         old_tags := prev.value FROM history_changes prev WHERE (prev.id = new.previous_id) LIMIT 1;
         old_tags_array := regexp_split_to_array(COALESCE(old_tags, ''), ' ');
         new_tags_array := regexp_split_to_array(COALESCE(new.value, ''), ' ');
@@ -93,9 +93,9 @@ CREATE FUNCTION history_changes_index_trigger() RETURNS trigger
         changed_tags_array := get_new_tags(old_tags_array, new_tags_array);
         changed_tags_array := array_cat(changed_tags_array, get_new_tags(new_tags_array, old_tags_array));
         indexed_value := join_string(changed_tags_array, ' ');
-      ELSEIF (new.table_name, new.field) IN (('pools', 'name')) THEN
+      ELSEIF (new.table_name, new.column_name) IN (('pools', 'name')) THEN
         indexed_value := translate(new.value, '_', ' ');
-      ELSEIF (new.table_name, new.field) IN (('posts', 'cached_tags'), ('posts', 'source'), ('pools', 'description'), ('notes', 'body')) THEN
+      ELSEIF (new.table_name, new.column_name) IN (('posts', 'cached_tags'), ('posts', 'source'), ('pools', 'description'), ('notes', 'body')) THEN
         indexed_value := new.value;
       ELSE
         RETURN new;
@@ -3597,6 +3597,8 @@ INSERT INTO schema_migrations (version) VALUES ('20120505130017');
 INSERT INTO schema_migrations (version) VALUES ('20120624121058');
 
 INSERT INTO schema_migrations (version) VALUES ('20120723155345');
+
+INSERT INTO schema_migrations (version) VALUES ('20120723161914');
 
 INSERT INTO schema_migrations (version) VALUES ('21');
 

b2d052b7ccb927be1dfeca6036e75f428d130d5f
Edho Arief <edho@myconan.net>
2012-07-23 23:50:31 +0200

    Simplified tag_version expiry method.

--- lib/core_ext/active_support_cache_store_ext.rb
+++ lib/core_ext/active_support_cache_store_ext.rb
@@ -8,12 +8,7 @@ module ActiveSupport
       def expire_tag_version
         # $tag_version is bumped when the type of a tag is changed in Tags, if
         # a new tag is created, or if a tag's post_count becomes nonzero.
-        incr("$tag_version")
-      end
-
-      def incr(key)
-          val = read(key)
-          write(key, val.to_i + 1)
+        write('$cache_version', Time.now.to_i)
       end
     end
     Store.send :include, StoreExt

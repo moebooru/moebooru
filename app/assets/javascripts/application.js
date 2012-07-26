f3786721602814e61c9eec23d13822adf6d03830
Edho Arief <edho@myconan.net>
2012-07-26 15:43:33 +0200

    Prepare to use jQuery.
    --HG--
    rename : app/assets/javascripts/browser-img-pool.js => lib/assets/javascripts/prototype-legacy/browser-img-pool.js
    rename : app/assets/javascripts/browser-post-loading.js => lib/assets/javascripts/prototype-legacy/browser-post-loading.js
    rename : app/assets/javascripts/browser-thumb-view.js => lib/assets/javascripts/prototype-legacy/browser-thumb-view.js
    rename : app/assets/javascripts/browser-view.js => lib/assets/javascripts/prototype-legacy/browser-view.js
    rename : app/assets/javascripts/comment.js => lib/assets/javascripts/prototype-legacy/comment.js
    rename : app/assets/javascripts/common.js => lib/assets/javascripts/prototype-legacy/common.js
    rename : app/assets/javascripts/cookie.js => lib/assets/javascripts/prototype-legacy/cookie.js
    rename : app/assets/javascripts/cropper.js => lib/assets/javascripts/prototype-legacy/cropper.js
    rename : app/assets/javascripts/debug-window.js => lib/assets/javascripts/prototype-legacy/debug-window.js
    rename : app/assets/javascripts/dmail.js => lib/assets/javascripts/prototype-legacy/dmail.js
    rename : app/assets/javascripts/favorite.js => lib/assets/javascripts/prototype-legacy/favorite.js
    rename : app/assets/javascripts/forum.js => lib/assets/javascripts/prototype-legacy/forum.js
    rename : app/assets/javascripts/history.js => lib/assets/javascripts/prototype-legacy/history.js
    rename : app/assets/javascripts/inline_image.js => lib/assets/javascripts/prototype-legacy/inline_image.js
    rename : app/assets/javascripts/main_menu.js => lib/assets/javascripts/prototype-legacy/main_menu.js
    rename : app/assets/javascripts/notes.js => lib/assets/javascripts/prototype-legacy/notes.js
    rename : app/assets/javascripts/pool.js => lib/assets/javascripts/prototype-legacy/pool.js
    rename : app/assets/javascripts/post-frame-editor.js => lib/assets/javascripts/prototype-legacy/post-frame-editor.js
    rename : app/assets/javascripts/post-upload-form.js => lib/assets/javascripts/prototype-legacy/post-upload-form.js
    rename : app/assets/javascripts/post.js => lib/assets/javascripts/prototype-legacy/post.js
    rename : app/assets/javascripts/post_mode_menu.js => lib/assets/javascripts/prototype-legacy/post_mode_menu.js
    rename : app/assets/javascripts/post_tag_history.js => lib/assets/javascripts/prototype-legacy/post_tag_history.js
    rename : app/assets/javascripts/preload.js => lib/assets/javascripts/prototype-legacy/preload.js
    rename : app/assets/javascripts/referral.js => lib/assets/javascripts/prototype-legacy/referral.js
    rename : app/assets/javascripts/related_tags.js => lib/assets/javascripts/prototype-legacy/related_tags.js
    rename : app/assets/javascripts/similar-thumbnailing.js => lib/assets/javascripts/prototype-legacy/similar-thumbnailing.js
    rename : app/assets/javascripts/tag_completion.js => lib/assets/javascripts/prototype-legacy/tag_completion.js
    rename : app/assets/javascripts/touchscreen-web-app-helpers.js => lib/assets/javascripts/prototype-legacy/touchscreen-web-app-helpers.js
    rename : app/assets/javascripts/url-hash.js => lib/assets/javascripts/prototype-legacy/url-hash.js
    rename : app/assets/javascripts/user.js => lib/assets/javascripts/prototype-legacy/user.js
    rename : app/assets/javascripts/user_record.js => lib/assets/javascripts/prototype-legacy/user_record.js
    rename : app/assets/javascripts/vote-widget.js => lib/assets/javascripts/prototype-legacy/vote-widget.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,45 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+//
 //= require prototype
 //= require effects
 //= require dragdrop
 //= require controls
 //= require builder
 //= require rails
+//= require prototype-legacy/browser-img-pool
+//= require prototype-legacy/browser-post-loading
+//= require prototype-legacy/browser-thumb-view
+//= require prototype-legacy/browser-view
+//= require prototype-legacy/comment
+//= require prototype-legacy/common
+//= require prototype-legacy/cookie
+//= require prototype-legacy/cropper
+//= require prototype-legacy/debug-window
+//= require prototype-legacy/dmail
+//= require prototype-legacy/favorite
+//= require prototype-legacy/forum
+//= require prototype-legacy/history
+//= require prototype-legacy/inline_image
+//= require prototype-legacy/main_menu
+//= require prototype-legacy/notes
+//= require prototype-legacy/pool
+//= require prototype-legacy/post-frame-editor
+//= require prototype-legacy/post-upload-form
+//= require prototype-legacy/post
+//= require prototype-legacy/post_mode_menu
+//= require prototype-legacy/post_tag_history
+//= require prototype-legacy/preload
+//= require prototype-legacy/referral
+//= require prototype-legacy/related_tags
+//= require prototype-legacy/similar-thumbnailing
+//= require prototype-legacy/tag_completion
+//= require prototype-legacy/touchscreen-web-app-helpers
+//= require prototype-legacy/url-hash
+//= require prototype-legacy/user
+//= require prototype-legacy/user_record
+//= require prototype-legacy/vote-widget
+//= require jquery
+//=# require jquery_ujs
 //= require_tree .
+jQuery.noConflict();

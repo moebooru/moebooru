de68d7a4baecebc46ee2d4aec97e73ad393ce9cd
Nurahmadie <nurahmadie@gmail.com>
2012-08-14 02:52:58 +0700

    Move post.js codebase to pure javascript
    We still have to enclose Post class inside function,
    since we don't want to contaminate previous Post implementation
    yet.

---
+++ app/assets/javascripts/post.js
@@ -0,0 +1,59 @@
+(function () {
+  var Post = function () {
+    this.posts = {};
+  };
+
+  Post.prototype.registerPosts = function (posts) {
+    var th = this;
+    if (posts.length == 1) {
+      this.current = posts[0];
+    }
+    posts.forEach(function(p, idx, arr) {
+      p.tags = p.tags.match(/\S+/g) || [];
+      p.metatags = p.tags.clone();
+      p.metatags.push("rating:" + p.rating[0]);
+      p.metatags.push("status:" + p.status);
+      th.posts[p.id] = p;
+    });
+    return false;
+  };
+
+  Post.prototype.get = function (post_id) {
+    return this.posts[post_id];
+  };
+
+  jQuery(function($) {
+    var post = new Post(),
+      inLargerVersion = false;
+    Moebooru.attach('post', post);
+
+    Moe.on('post:add', function (e, data) {
+      post.registerPosts(data);
+    });
+
+    $('.highres-show').on('click', function () {
+      var img = $('#image'),
+        w = img.attr('large_width'),
+        h = img.attr('large_height');
+      if (inLargerVersion) { return false; }
+      if (window.Note) {
+        window.Note.all.invoke('adjustScale');
+      }
+      inLargerVersion = true;
+      $('#resized_notice').hide();
+      img.hide();
+      img.attr('src', '');
+      img.attr('width', w);
+      img.attr('height', h);
+      img.attr('src', this.href);
+      img.show();
+      return false;
+    });
+
+    $('#post_tags').on('keydown', function (e) {
+      if (e.which == 13) {
+        $('#edit-form').submit();
+      }
+    });
+  });
+})();

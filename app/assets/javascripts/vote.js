4fe1f0f09c701e6508797872029b4a1de8fdefd1
Nurahmadie <nurahmadie@gmail.com>
2012-08-14 21:08:38 +0700

    vote.js instead of vote.js.coffee

---
+++ app/assets/javascripts/vote.js
@@ -0,0 +1,161 @@
+(function() {
+  var Vote = function () {
+    this.api = {
+      set: '/post/vote.json'
+    };
+    this.v = {
+      fav: 3,
+      great: 2,
+      good: 1,
+      remove: 0
+    };
+    this.label = [
+      t('vote.remove'), t('vote.good'), t('vote.great'), t('vote.fav')
+    ];
+  };
+
+  Vote.prototype.registerVotes = function (votes) {
+    this.posts = votes;
+  };
+
+  Vote.prototype.registerUserVotes = function (votes) {
+    this.votes = votes;
+  };
+
+  Vote.prototype.getScore = function () {
+    this.current_post = Moebooru.get('post').current;
+    try {
+      return this.current_post.score;
+    } catch (error) {}
+    return null;
+  };
+
+  Vote.prototype.getVote = function () {
+    return this.posts[this.current_post.id] || 0;
+  };
+
+  Vote.prototype.set = function (vote) {
+    if (vote > this.v.fav) return false;
+    notice(t('voting') + '...');
+    Moebooru.request(this.api.set, {id: this.current_post.id, score: vote});
+    return false;
+  };
+
+  Vote.prototype.updateWidget = function () {
+    var $ = jQuery,
+        score = this.getScore(),
+        vote = this.getVote();
+    for (var i = 1; i <= this.v.fav; i++) {
+      var star = $('.star-'+i);
+      if (i <= vote) {
+        star.removeClass('star-set-after');
+        star.addClass('star-set-upto');
+      } else {
+        star.removeClass('star-set-upto');
+        star.addClass('star-set-after');
+      }
+    }
+    if (vote === this.v.fav) {
+      $('#add-to-favs').css('display', 'none');
+      $('#remove-from-favs').css('display', 'list-item');
+    } else {
+      $('#add-to-favs').css('display', 'list-item');
+      $('#remove-from-favs').css('display', 'none');
+    }
+    $('#post-score-'+this.current_post.id).html(score);
+    try {
+      $('#favorited-by').html(Favorite.link_to_users(this.votes[this.v.fav]));
+    } catch (error) {}
+    return false;
+  };
+
+  jQuery(function($) {
+    var container = $('#stats'),
+        star = $('.star-off'),
+        vote = new Vote();
+
+    if (container.length === 0) return false;
+
+    function get_score(o) {
+      var match = o.match(/star\-(\d)/);
+      try {
+        if (match.length === 2) {
+          return parseInt(match[1]);
+        }
+      } catch (error) {}
+      return -1;
+    }
+
+    Moe.on('vote:add', function (e, data) {
+      vote.registerVotes(data);
+    });
+
+    Moe.on('vote:add_user_list', function (e, data) {
+      vote.registerUserVotes(data);
+    });
+
+    Moe.on(vote.api.set+':ready', function (e, data) {
+      notice(t('vote_saved'));
+      Moebooru.addData(data);
+      vote.updateWidget();
+    });
+
+    Moe.on('vote:update_widget', function () {
+      vote.updateWidget();
+    });
+
+    $('#add-to-favs').on('click', function () {
+      return vote.set(vote.v.fav);
+    });
+
+    $('#remove-from-favs').on('click', function () {
+      return vote.set(vote.v.great);
+    });
+
+    $('.vote-up').on('click', function () {
+      var current_score = vote.getVote();
+      if (current_score === vote.v.fav) return false;
+      return vote.set(current_score + 1);
+    });
+
+    star.on('click', function () {
+      var score = get_score(this.className);
+      return vote.set(score);
+    });
+
+    star.on('mouseover', function () {
+      var score = get_score(this.className);
+      for (var i = 1; i <= vote.v.fav; i++) {
+        var star = $('.star-'+i);
+        if (i <= score) {
+          star.removeClass('star-hovered-after');
+          star.addClass('star-hovered-upto');
+        } else {
+          star.removeClass('star-hovered-upto');
+          star.addClass('star-hovered-after');
+        }
+        if (i != score) {
+          star.removeClass('star-hovered');
+          star.addClass('star-unhovered');
+        } else {
+          star.removeClass('star-unhovered');
+          star.removeClass('star-hovered');
+        }
+      }
+      $('.vote-desc').html(vote.label[score]);
+      return false;
+    });
+
+    star.on('mouseout', function () {
+      for (var i = 1; i <= vote.v.fav; i++) {
+        var star = $('.star-'+i);
+        star.removeClass('star-hovered');
+        star.removeClass('star-unhovered');
+        star.removeClass('star-hovered-after');
+        star.removeClass('star-hovered-upto');
+      }
+      $('.vote-desc').html('');
+    });
+
+  });
+})();

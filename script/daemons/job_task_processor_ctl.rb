d0be95b8b3b84734788fc609b73e2336e41f4fc7
Edho Arief <edho@myconan.net>
2012-08-25 18:54:02 -0700

    Explicit daemons gem require. Not sure why.
    Doesn't work without it in rbx.

--- script/daemons/job_task_processor_ctl.rb
+++ script/daemons/job_task_processor_ctl.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'daemons'
+
 ENV['NEWRELIC_ENABLE'] = 'false'
 Daemons.run(File.expand_path('../job_task_processor.rb', __FILE__), :log_output => true, :dir => File.expand_path('../../../log', __FILE__))

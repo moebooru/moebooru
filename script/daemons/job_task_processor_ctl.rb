3564aff60cff4e2a32b47c77e3a42a71eb8f6adf
Edho Arief <edho@myconan.net>
2012-08-25 20:34:09 -0700

    Daemons require :dir_mode
    Also cleaned up the script.

--- script/daemons/job_task_processor_ctl.rb
+++ script/daemons/job_task_processor_ctl.rb
@@ -3,4 +3,11 @@
 require 'daemons'
 
 ENV['NEWRELIC_ENABLE'] = 'false'
-Daemons.run(File.expand_path('../job_task_processor.rb', __FILE__), :log_output => true, :dir => File.expand_path('../../../log', __FILE__))
+job_script = File.expand_path('../job_task_processor.rb', __FILE__)
+job_options = {
+  :dir_mode => :normal,
+  :dir => File.expand_path('../../../log', __FILE__),
+  :log_output => true,
+  :multiple => false
+}
+Daemons.run job_script, job_options

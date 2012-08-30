20037b05a63581f1a0f4921a1cf4961a665166be
Edho Arief <edho@myconan.net>
2012-08-30 04:09:28 -0700

    Fixes ArgumentError when accessing with params like %81=a.
    --HG--
    branch : 3.2.0-release

---
+++ lib/core_ext/rack_utils.rb
@@ -0,0 +1,12 @@
+require 'rack/utils'
+
+module Rack
+  module Utils
+    def normalize_params_with_sanitation(params, name, v = nil)
+      ActionDispatch::Encoder.encode_to_internal(name)
+      normalize_params_without_sanitation(params, name, v = nil)
+    end
+    alias_method_chain :normalize_params, :sanitation
+    module_function :normalize_params, :normalize_params_with_sanitation, :normalize_params_without_sanitation
+  end
+end

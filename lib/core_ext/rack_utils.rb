7930498287bec3eb21ed513627585327938ea433
Edho Arief <edho@myconan.net>
2012-08-30 04:40:00 -0700

    Accidentally nulled the values :D
    --HG--
    branch : 3.2.0-release

--- lib/core_ext/rack_utils.rb
+++ lib/core_ext/rack_utils.rb
@@ -4,7 +4,7 @@ module Rack
   module Utils
     def normalize_params_with_sanitation(params, name, v = nil)
       ActionDispatch::Encoder.encode_to_internal(name)
-      normalize_params_without_sanitation(params, name, v = nil)
+      normalize_params_without_sanitation(params, name, v)
     end
     alias_method_chain :normalize_params, :sanitation
     module_function :normalize_params, :normalize_params_with_sanitation, :normalize_params_without_sanitation

2e1216e70acc23fcd68888d8c7f0bb4bc39bb630
Edho Arief <edho@myconan.net>
2012-08-30 18:57:40 +0700

    Use method chaining instead of orig_ prefix.

--- lib/core_ext/patch-remove_commit_search_from_form.rb
+++ lib/core_ext/patch-remove_commit_search_from_form.rb
@@ -4,13 +4,13 @@ module ActionView
     module TagHelper
       # submit_tag "Search" generates a submit tag that adds "commit=Search" to the URL,
       # which is ugly and unnecessary.  Override TagHelper#tag and remove this globally.
-      alias_method :orig_tag, :tag
-      def tag(name, options = nil, open = false, escape = true)
+      def tag_with_remove_commit_search(name, options = nil, open = false, escape = true)
         if name == :input && options['type'] == 'submit' && options['name'] == 'commit' && options['value'] == 'Search'
           options.delete 'name'
         end
-        orig_tag name, options, open, escape
+        tag_without_remove_commit_search name, options, open, escape
       end
+      alias_method_chain :tag, :remove_commit_search
     end
   end
 end

9e969e710be4eac7e993531ebab71c95309d5e9d
Edho Arief <edho@myconan.net>
2012-09-16 00:17:45 +0700

    Prefix loaded earliest.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 //
+//= require prefix
 //= require prototype-legacy/prototype
 //= require prototype-legacy/effects
 //= require prototype-legacy/dragdrop

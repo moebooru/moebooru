8ffe3482d5620d15cdea5d6b37acc40edc0fc4bb
Edho Arief <edho@myconan.net>
2012-09-15 22:27:57 +0700

    Added some documentation.

--- app/assets/javascripts/cookie-setup.js
+++ app/assets/javascripts/cookie-setup.js
@@ -1,8 +1,10 @@
 jQuery(document).ready(function($) {
+  // Check if there's new dmail.
   if ($.cookie('has_mail') == '1') {
     $('#has-mail-notice').show();
   };
 
+  // Check if there's any pending post moderation queue.
   if (parseInt($.cookie('mod_pending')) > 0) {
     $('#moderate').addClass('mod-pending');
   };

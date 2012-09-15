507148e55071e0f1af2e777d3f411f7c5d15e205
Edho Arief <edho@myconan.net>
2012-09-15 22:27:02 +0700

    Moved mod_pending check to jquery.

--- app/assets/javascripts/cookie-setup.js
+++ app/assets/javascripts/cookie-setup.js
@@ -1,5 +1,9 @@
 jQuery(document).ready(function($) {
   if ($.cookie('has_mail') == '1') {
     $('#has-mail-notice').show();
-  }
+  };
+
+  if (parseInt($.cookie('mod_pending')) > 0) {
+    $('#moderate').addClass('mod-pending');
+  };
 });

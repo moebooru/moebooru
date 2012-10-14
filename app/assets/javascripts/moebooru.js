37a36853a21eecc30a1ee21ecbc22cd0b33d9379
Nurahmadie <nurahmadie@gmail.com>
2012-10-14 11:22:14 +0700

    Remove unused code

--- app/assets/javascripts/moebooru.js
+++ app/assets/javascripts/moebooru.js
@@ -1,58 +1,10 @@
 (function ($) {
-  Moebooru = {
-    '__slug__': {}
-  };
+  Moebooru = {};
   Moe = $(Moebooru);
-
-  Moebooru.addData = function (data) {
-    if (data.posts)
-      Moe.trigger("post:add", [data.posts]);
-    if (data.tags)
-      Moe.trigger("tag:add", [data.tags]);
-    if (data.votes)
-      Moe.trigger("vote:add", [data.votes]);
-    if (data.voted_by)
-      Moe.trigger("vote:add_user_list", [data.voted_by]);
-    if (data.pools)
-      Moe.trigger("pool:add", [data.pools]);
-    if (data.pool_posts)
-      Moe.trigger("pool:add_post", {
-        pool_posts: data.pool_posts,
-        posts: data.posts
-      });
-  };
-
-  Moebooru.attach = function (key, obj) {
-    Moebooru['__slug__'][key] = obj;
-  };
-
-  Moebooru.get = function (key) {
-   return Moebooru['__slug__'][key];
-  };
-
-  Moebooru.request = function (url, params/*, type */) {
-    $.ajax({
-      url: Moebooru.path(url),
-      type: arguments[2] || 'POST',
-      dataType: 'json',
-      data: params,
-      statusCode: {
-        403: function () {
-          notice(t('error')+': '+t('denied'));
-        }
-      }
-    }).done(function (data) {
-      Moe.trigger(url+":ready", [data]);
-    }).fail(function () {
-      notice(t('error'));
-    });
-  };
 
   Moebooru.path = function (url) {
     return PREFIX === '/' ? url : PREFIX + url;
   }
-
-
 
   // XXX: Tested on chrome, mozilla, msie(9/10)
   // might or might not works in other browser

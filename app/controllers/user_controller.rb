f0d3d2625ed2ac778ce4da6a04bd466aa5f3dc1a
dovac <dobacco@gmail.com>
2012-10-17 16:47:34 -0700

    add HttpOnly ref: http://www.codinghorror.com/blog/2008/08/protecting-your-cookies-httponly.html
    --HG--
    branch : 3.2.0-release

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -14,7 +14,7 @@ class UserController < ApplicationController
   protected
   def save_cookies(user)
     cookies[:login] = {:value => user.name, :expires => 1.year.from_now}
-    cookies[:pass_hash] = {:value => user.password_hash, :expires => 1.year.from_now}
+    cookies[:pass_hash] = {:value => user.password_hash, :expires => 1.year.from_now, :httponly => true}
     session[:user_id] = user.id
   end
 

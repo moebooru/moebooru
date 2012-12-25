e422f853eb8a09d62415e8fc48e3e5a988f314c4
Edho Arief <edho@myconan.net>
2012-12-25 18:38:39 +0700

    Refactored the menu initialization.

--- app/assets/javascripts/init.menu.js
+++ app/assets/javascripts/init.menu.js
@@ -1,31 +1,3 @@
 jQuery(document).ready(function($) {
-  var menu = $('#main-menu');
-  // Highlight current location (based on controller)
-  var hl_menu_class = '.' + menu.data('controller');
-  menu.find(hl_menu_class).addClass('current-menu');
-
-  // Add 5 latest forum topics to forum submenu
-  var forum_menu_items = $.parseJSON($.cookie('current_forum_posts'));
-  var create_forum_item = function(forum_json) {
-    return $('<li/>', {
-      html: $('<a/>', {
-        href: Moebooru.path('/forum/show/' + forum_json[1]),
-        text: forum_json[0],
-        class: forum_json[2] ? 'unread-topic' : ' '
-      }),
-    });
-  };
-  var menu_items_num = forum_menu_items.length > 5 ? 5 : forum_menu_items.length;
-  for (var i = menu_items_num - 1; i >=0; i--) {
-    $('.forum-items-start').after(create_forum_item(forum_menu_items[i]));
-  };
-  var forum_submenu = menu.find('.forum ul');
-  if (forum_submenu.width() > 200) {
-    forum_submenu.css('width', 200)
-    forum_submenu.find('a').css('white-space', 'normal');
-  };
-
-  // Shows #cn
-  // FIXME: I have no idea what this is for.
-  $('#cn').show();
+  Menu.init();
 });
--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -6,4 +6,53 @@
     Forum.mark_all_read();
     return false;
   });
+
+  Menu = {
+    menu: null,
+
+    // Highlight current location (based on controller)
+    set_highlight: function() {
+      var hl_menu_class = '.' + this.menu.data('controller');
+      this.menu.find(hl_menu_class).addClass('current-menu');
+    },
+
+    /*
+     * Sets various forum-related menu:
+     * - adds 5 latest topics
+     * - set correct class based on read/unread
+     */
+    sync_forum_menu: function() {
+      var forum_menu_items = $.parseJSON($.cookie('current_forum_posts'));
+      var create_forum_item = function(forum_json) {
+        return $('<li/>', {
+          html: $('<a/>', {
+            href: Moebooru.path('/forum/show/' + forum_json[1]),
+            text: forum_json[0],
+            class: forum_json[2] ? 'unread-topic' : ' '
+          }),
+        });
+      };
+      this.menu.find('.forum-items-start').nextAll().remove();
+      var menu_items_num = forum_menu_items.length > 5 ? 5 : forum_menu_items.length;
+      for (var i = menu_items_num - 1; i >=0; i--) {
+        this.menu.find('.forum-items-start').after(create_forum_item(forum_menu_items[i]));
+      };
+      var forum_submenu = this.menu.find('.forum ul');
+      if (forum_submenu.width() > 200) {
+        forum_submenu.css('width', 200)
+        forum_submenu.find('a').css('white-space', 'normal');
+      };
+    },
+
+    init: function() {
+      this.menu = $('#main-menu');
+      this.set_highlight();
+      this.sync_forum_menu();
+      /*
+       * Shows #cn
+       * FIXME: I have no idea what this is for.
+       */
+      $('#cn').show();
+    }
+  };
 }) (jQuery);

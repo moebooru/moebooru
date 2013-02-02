09e9dfbc790f74461dffb514fd4a4c94a8a4f9ae
Nurahmadie <nurahmadie@gmail.com>
2013-02-02 22:43:13 +0700

    Require I18n-js at application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,6 +8,9 @@
 //= require jquery.ui.autocomplete
 //= require compat.jquery
 //= require mousetrap
+//= require i18n
+//= i18n/translations
+//= require i18n_scopify
 //= require cookie
 //= require dmail
 //= require favorite

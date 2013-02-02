753a143af78d4c7d0c155b67d2687445aef6bf06
Nurahmadie <nurahmadie@gmail.com>
2013-02-02 22:39:30 +0700

    Add i18n-js config and translation files

---
+++ app/assets/javascripts/i18n/translations.js
@@ -0,0 +1,2 @@
+var I18n = I18n || {};
+I18n.translations = {"en":{"js":{"comment":{"flag_ask":"Flag this comment?","flag_process":"Flagging comment for deletion...","flag_notice":"Comment flagged for deletion","quote_error":"Error quoting comment","delete_ask":"Are you sure you want to delete this comment?","delete_error":"Error deleting comment: "},"denied":"Access Denied","error":"Error: ","no_translation":"No translation: ","vote":{"remove":"Remove vote","good":"Good","great":"Great","fav":"Favorite","saved":"Vote saved","voting":"Voting..."}}}};

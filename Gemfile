c0f30a84346987a99e68a0983b743919b751dad7
Edho Arief <edho@myconan.net>
2013-04-17 22:07:23 -0700

    Rollback the escape_utils

--- Gemfile
+++ Gemfile
@@ -50,4 +50,3 @@ end
 gem 'oj', :platforms => :ruby
 gem 'multi_json'
 gem 'jbuilder'
-gem 'escape_utils', :platforms => :mri
--- config/initializers/escape_utils.rb
+++
@@ -1,6 +0,0 @@
-if defined? EscapeUtils
-  require 'escape_utils/html/rack' # to patch Rack::Utils
-  require 'escape_utils/html/erb' # to patch ERB::Util
-  require 'escape_utils/html/cgi' # to patch CGI
-  require 'escape_utils/html/haml' # to patch Haml::Helpers
-end

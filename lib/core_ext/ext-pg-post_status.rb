2949db902148bba0f83f9fa8a2b80b4b786d40ec
edogawaconan <me@myconan.net>
2013-07-14 13:44:16 +0900

    Add post_status datatype.

---
+++ lib/core_ext/ext-pg-post_status.rb
@@ -0,0 +1,11 @@
+require "active_record/connection_adapters/abstract_adapter"
+
+module ActiveRecord
+  module ConnectionAdapters
+    class PostgreSQLAdapter < AbstractAdapter
+      module OID
+        alias_type "post_status", "text"
+      end
+    end
+  end
+end

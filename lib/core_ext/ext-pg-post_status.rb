1ccd6d2e4c089a195ac93124f9e3969bf9748e62
edogawaconan <me@myconan.net>
2013-08-12 20:52:53 +0900

    The post_status thing only required on pg.
    Maybe.

--- lib/core_ext/ext-pg-post_status.rb
+++ lib/core_ext/ext-pg-post_status.rb
@@ -1,10 +1,12 @@
-require "active_record/connection_adapters/abstract_adapter"
+if RUBY_PLATFORM != "java"
+  require "active_record/connection_adapters/abstract_adapter"
 
-module ActiveRecord
-  module ConnectionAdapters
-    class PostgreSQLAdapter < AbstractAdapter
-      module OID
-        alias_type "post_status", "text"
+  module ActiveRecord
+    module ConnectionAdapters
+      class PostgreSQLAdapter < AbstractAdapter
+        module OID
+          alias_type "post_status", "text"
+        end
       end
     end
   end

84de9ead30ec7163457f99d7c5fbe63e0a4cee19
edogawaconan <me@myconan.net>
2014-01-21 11:42:37 +0900

    Actually compress javascript.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,8 @@ Moebooru::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false

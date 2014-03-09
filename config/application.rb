2c873cc4e61c1118db20fbd794ef0313449c1bd4
edogawaconan <me@myconan.net>
2014-03-09 16:45:25 +0900

    Enable locale enforcer.

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,7 @@ module Moebooru
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    config.i18n.enforce_available_locales = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = CONFIG['default_locale']

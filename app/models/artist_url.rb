8aa96f145217b2aedb95c6a96d593aa50f1a22a3
edogawaconan <me@myconan.net>
2014-08-23 18:57:09 +0900

    Refactor artist url normalize.
    Less variable assignment.

--- app/models/artist_url.rb
+++ app/models/artist_url.rb
@@ -3,19 +3,14 @@ class ArtistUrl < ActiveRecord::Base
   validates_presence_of :url
 
   def self.normalize(url)
-    if url.nil?
-      return nil
-    else
-      url = url.gsub(/^http:\/\/blog\d+\.fc2/, "http://blog.fc2")
-      url = url.gsub(/^http:\/\/blog-imgs-\d+\.fc2/, "http://blog.fc2")
-      # Pixiv pre-2012
-      # Example URL: http://img55.pixiv.net/img/kazamatuli/16634039.jpg
-      url = url.gsub(/^http:\/\/img\d+\.pixiv\.net/, "http://img.pixiv.net")
-      # Pixiv 2012
-      # Example URL: http://i1.pixiv.net/img55/img/kazamatuli/29701368.jpg
-      url = url.gsub(/^http:\/\/i\d+\.pixiv\.net\/img\d+/, "http://img.pixiv.net")
-      return url
-    end
+    return unless url
+
+    url.gsub(/^http:\/\/blog\d+\.fc2/, "http://blog.fc2")
+      .gsub(/^http:\/\/blog-imgs-\d+\.fc2/, "http://blog.fc2")
+      .gsub(/^http:\/\/img\d+\.pixiv\.net/, "http://img.pixiv.net") # [1]
+      .gsub(/^http:\/\/i\d+\.pixiv\.net\/img\d+/, "http://img.pixiv.net") # [2]
+    # [1] pixiv pre-2012. Example URL: http://img55.pixiv.net/img/kazamatuli/16634039.jpg
+    # [2] pixiv 2012. Example URL: http://i1.pixiv.net/img55/img/kazamatuli/29701368.jpg
   end
 
   def self.normalize_for_search(url)

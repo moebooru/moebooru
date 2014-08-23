a2543b3a2b9afbc5fbf8201d54c6339f46cde28e
edogawaconan <me@myconan.net>
2014-08-24 01:11:52 +0900

    Fix test for UserRecord.

--- test/unit/user_record_test.rb
+++ test/unit/user_record_test.rb
@@ -1,9 +1,9 @@
-require File.dirname(__FILE__) + "/../test_helper"
+require "test_helper"
 
 class UserRecordTest < ActiveSupport::TestCase
   def setup
     if CONFIG["enable_caching"]
-      CACHE.flush_all
+      Rails.cache.clear
     end
   end
 
@@ -18,6 +18,6 @@ class UserRecordTest < ActiveSupport::TestCase
   def test_all
     dmail = UserRecord.create(:body => "bad", :user => "member", :reported_by => 1, :is_positive => true)
     assert_equal(4, dmail.user_id)
-    assert_not_nil(Dmail.find_by_body("admin created a positive record for your account. <a href=\"/user_record/index?user_id=4\">View your record</a>."))
+    assert_not_nil(Dmail.find_by_body("admin created a positive record for your account. <<http://#{CONFIG["server_host"]}/user_record?user_id=4|View your record>>."))
   end
 end

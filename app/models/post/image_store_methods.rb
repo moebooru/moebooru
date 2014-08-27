79233cda0c721a64b4a32853b7dd8a052fe27c00
edogawaconan <me@myconan.net>
2014-08-27 16:06:07 +0900

    Nuke aws storage system. Not used anymore.
    --HG--
    extra : amend_source : c4faa3651b11a67a2942caadb23a9324a98161ca

--- app/models/post/image_store_methods.rb
+++ app/models/post/image_store_methods.rb
@@ -4,17 +4,11 @@ module Post::ImageStoreMethods
     when :local_flat
       m.__send__(:include, Post::ImageStore::LocalFlat)
 
-    when :local_flat_with_amazon_s3_backup
-      m.__send__(:include, Post::ImageStore::LocalFlatWithAmazonS3Backup)
-
     when :local_hierarchy
       m.__send__(:include, Post::ImageStore::LocalHierarchy)
 
     when :remote_hierarchy
       m.__send__(:include, Post::ImageStore::RemoteHierarchy)
-
-    when :amazon_s3
-      m.__send__(:include, Post::ImageStore::AmazonS3)
     end
   end
 

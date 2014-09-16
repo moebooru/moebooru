04b98abd0f7f3b955132d6f58de4ea24654d1185
edogawaconan <me@myconan.net>
2014-09-16 16:24:28 +0900

    Fix test for ArtistUrl.

--- test/unit/artist_url_test.rb
+++ test/unit/artist_url_test.rb
@@ -5,31 +5,32 @@ class ArtistUrlTest < ActiveSupport::TestCase
 
   def setup
     if CONFIG["enable_caching"]
-      CACHE.flush_all
+      Rails.cache.clear
     end
   end
 
   def test_normalize
-    url = ArtistUrl.create(:artist_id => 1, :url => nil)
+    artist = Artist.create(:name => "yukie")
+    url = ArtistUrl.create(:artist_id => artist.id, :url => nil)
     assert !url.valid?
 
-    url = ArtistUrl.create(:artist_id => 1, :url => "http://monet.com")
+    url = ArtistUrl.create(:artist_id => artist.id, :url => "http://monet.com")
     assert_equal("http://monet.com", url.url)
     assert_equal("http://monet.com", url.normalized_url)
 
-    url = ArtistUrl.create(:artist_id => 1, :url => "http://monet.com/")
+    url = ArtistUrl.create(:artist_id => artist.id, :url => "http://monet.com/")
     assert_equal("http://monet.com/", url.url)
     assert_equal("http://monet.com/", url.normalized_url)
 
-    url = ArtistUrl.create(:artist_id => 1, :url => "http://blog55.fc2.com/monet")
+    url = ArtistUrl.create(:artist_id => artist.id, :url => "http://blog55.fc2.com/monet")
     assert_equal("http://blog55.fc2.com/monet", url.url)
     assert_equal("http://blog.fc2.com/monet", url.normalized_url)
 
-    url = ArtistUrl.create(:artist_id => 1, :url => "http://blog-imgs-55.fc2.com/monet")
+    url = ArtistUrl.create(:artist_id => artist.id, :url => "http://blog-imgs-55.fc2.com/monet")
     assert_equal("http://blog-imgs-55.fc2.com/monet", url.url)
     assert_equal("http://blog.fc2.com/monet", url.normalized_url)
 
-    url = ArtistUrl.create(:artist_id => 1, :url => "http://img55.pixiv.net/monet")
+    url = ArtistUrl.create(:artist_id => artist.id, :url => "http://img55.pixiv.net/monet")
     assert_equal("http://img55.pixiv.net/monet", url.url)
     assert_equal("http://img.pixiv.net/monet", url.normalized_url)
   end

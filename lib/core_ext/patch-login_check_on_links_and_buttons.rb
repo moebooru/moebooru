36e6c2173a824a06ac49ee0fe12b20472ee20e5f
edogawaconan <me@myconan.net>
2014-11-09 06:47:50 +0900

    Simplify login check injection.

--- lib/core_ext/patch-login_check_on_links_and_buttons.rb
+++ lib/core_ext/patch-login_check_on_links_and_buttons.rb
@@ -5,11 +5,11 @@ module ActionView
   module Helpers
     module TagHelper
       def tag_options_with_login_check(options, escape = true)
-        level = options["level"]
+        level = options.delete "level"
         if level && ApplicationHelper.need_signup?(level)
-          options.delete "level"
           options["onclick"] = "if(!User.run_login_onclick(event)) return false; #{options["onclick"] || "return true;"}"
         end
+
         tag_options_without_login_check options, escape
       end
       alias_method_chain :tag_options, :login_check
@@ -20,14 +20,11 @@ module ActionView
       # execute this action.  User.js uses this to determine whether it should ask
       # the user to create an account.
       def form_tag_with_login_check(url_for_options = {}, options = {}, *parameters_for_url, &block)
-        if options[:level]
-          if ApplicationHelper.need_signup?(options[:level])
-            classes = (options[:class] || "").split(" ")
-            classes += ["need-signup"]
-            options[:class] = classes.join(" ")
-          end
-          options.delete :level
+        level = options.delete :level
+        if level && ApplicationHelper.need_signup?(level)
+          options[:class] = "#{options[:class]} need-signup"
         end
+
         form_tag_without_login_check url_for_options, options, *parameters_for_url, &block
       end
       alias_method_chain :form_tag, :login_check
@@ -39,13 +36,12 @@ module ActionView
       # the user to create an account.
       def link_to_function_with_login_check(name, *args, &block)
         html_options = args.extract_options!
-        if html_options[:level]
-          if ApplicationHelper.need_signup?(html_options[:level]) && args[0]
-            args[0] = "User.run_login(false, function() { #{args[0]} })"
-          end
-          html_options.delete :level
+        level = html_options.delete :level
+        if level && ApplicationHelper.need_signup?(level) && args[0]
+          args[0] = "User.run_login(false, function() { #{args[0]} })"
         end
         args << html_options
+
         link_to_function_without_login_check name, *args, &block
       end
       alias_method_chain :link_to_function, :login_check
@@ -55,13 +51,12 @@ module ActionView
       # the user to create an account.
       def button_to_function_with_login_check(name, *args, &block)
         html_options = args.extract_options!
-        if html_options[:level]
-          if ApplicationHelper.need_signup?(html_options[:level]) && args[0]
-            args[0] = "User.run_login(false, function() { #{args[0]} })"
-          end
-          html_options.delete :level
+        level = html_options.delete :level
+        if level && ApplicationHelper.need_signup?(level) && args[0]
+          args[0] = "User.run_login(false, function() { #{args[0]} })"
         end
         args << html_options
+
         button_to_function_without_login_check name, *args, &block
       end
       alias_method_chain :button_to_function, :login_check

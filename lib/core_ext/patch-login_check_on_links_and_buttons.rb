82cb52fb5d4e206811115490ee13771947e8ee4e
edogawaconan <me@myconan.net>
2014-11-09 06:42:24 +0900

    `alias_method_chain` instead of `alias_method`.

--- lib/core_ext/patch-login_check_on_links_and_buttons.rb
+++ lib/core_ext/patch-login_check_on_links_and_buttons.rb
@@ -4,23 +4,22 @@
 module ActionView
   module Helpers
     module TagHelper
-      alias_method :orig_tag_options, :tag_options
-      def tag_options(options, escape = true)
+      def tag_options_with_login_check(options, escape = true)
         level = options["level"]
         if level && ApplicationHelper.need_signup?(level)
           options.delete "level"
           options["onclick"] = "if(!User.run_login_onclick(event)) return false; #{options["onclick"] || "return true;"}"
         end
-        orig_tag_options options, escape
+        tag_options_without_login_check options, escape
       end
+      alias_method_chain :tag_options, :login_check
     end
 
     module FormTagHelper
       # Add the need-signup class if signing up would allow a logged-out user to
       # execute this action.  User.js uses this to determine whether it should ask
       # the user to create an account.
-      alias_method :orig_form_tag, :form_tag
-      def form_tag(url_for_options = {}, options = {}, *parameters_for_url, &block)
+      def form_tag_with_login_check(url_for_options = {}, options = {}, *parameters_for_url, &block)
         if options[:level]
           if ApplicationHelper.need_signup?(options[:level])
             classes = (options[:class] || "").split(" ")
@@ -29,16 +28,16 @@ module ActionView
           end
           options.delete :level
         end
-        orig_form_tag url_for_options, options, *parameters_for_url, &block
+        form_tag_without_login_check url_for_options, options, *parameters_for_url, &block
       end
+      alias_method_chain :form_tag, :login_check
     end
 
     module JavaScriptHelper
       # Add the need-signup class if signing up would allow a logged-out user to
       # execute this action.  User.js uses this to determine whether it should ask
       # the user to create an account.
-      alias_method :orig_link_to_function, :link_to_function
-      def link_to_function(name, *args, &block)
+      def link_to_function_with_login_check(name, *args, &block)
         html_options = args.extract_options!
         if html_options[:level]
           if ApplicationHelper.need_signup?(html_options[:level]) && args[0]
@@ -47,14 +46,14 @@ module ActionView
           html_options.delete :level
         end
         args << html_options
-        orig_link_to_function name, *args, &block
+        link_to_function_without_login_check name, *args, &block
       end
+      alias_method_chain :link_to_function, :login_check
 
       # Add the need-signup class if signing up would allow a logged-out user to
       # execute this action.  User.js uses this to determine whether it should ask
       # the user to create an account.
-      alias_method :orig_button_to_function, :button_to_function
-      def button_to_function(name, *args, &block)
+      def button_to_function_with_login_check(name, *args, &block)
         html_options = args.extract_options!
         if html_options[:level]
           if ApplicationHelper.need_signup?(html_options[:level]) && args[0]
@@ -63,8 +62,9 @@ module ActionView
           html_options.delete :level
         end
         args << html_options
-        orig_button_to_function name, *args, &block
+        button_to_function_without_login_check name, *args, &block
       end
+      alias_method_chain :button_to_function, :login_check
     end
   end
 end

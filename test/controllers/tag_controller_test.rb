aa2a6c6f2b3e2f972b84da076c187185247de374
edogawaconan <me@myconan.net>
2014-11-20 22:22:32 +0900

    Set current user before creating post.
    When creating post with tag.

--- test/controllers/tag_controller_test.rb
+++ test/controllers/tag_controller_test.rb
@@ -4,6 +4,7 @@ class TagControllerTest < ActionController::TestCase
   fixtures :users
 
   def create_post(tags, post_number = 1, params = {})
+    Thread.current["danbooru-user_id"] = 1
     Post.create({ :user_id => 1, :score => 0, :source => "", :rating => "s", :width => 100, :height => 100, :ip_addr => "127.0.0.1", :updater_ip_addr => "127.0.0.1", :updater_user_id => 1, :tags => tags, :status => "active", :file => upload_file("#{Rails.root}/test/mocks/test/test#{post_number}.jpg") }.merge(params))
   end
 

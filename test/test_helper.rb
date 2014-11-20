bcb28996e640b56157215604e375096108ce3b61
edogawaconan <me@myconan.net>
2014-11-20 11:53:07 +0900

    Readd all fixtures.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ class ActiveSupport::TestCase
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
-  # fixtures :all
+  fixtures :all
 
   def upload_file(path)
     t = Tempfile.new File.basename(path)

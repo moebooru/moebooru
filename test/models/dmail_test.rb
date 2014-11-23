6691b9927f2416d4497539ed57908c8b8eb771b2
edogawaconan <me@myconan.net>
2014-11-23 13:09:44 +0900

    Fix test for Dmail model.

--- test/models/dmail_test.rb
+++ test/models/dmail_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class DmailTest < ActiveSupport::TestCase
+  include ActionMailer::TestHelper
   fixtures :users
 
   def setup
@@ -40,8 +41,12 @@ class DmailTest < ActiveSupport::TestCase
     assert_equal(4, msg.to_id)
     assert_equal(1, msg.from_id)
     assert_equal(true, User.find(4).has_mail?)
-    assert_equal(1, ActionMailer::Base.deliveries.size)
-    assert_equal("From: #{CONFIG["admin_contact"]}\r\nTo: member@danbooru.com\r\nSubject: #{CONFIG["app_name"]} - Message received from admin\r\nMime-Version: 1.0\r\nContent-Type: text/html; charset=utf-8\r\n\r\n<p>admin said:</p>\n\n<div>\n  <p>hello</p>\n</div>\n", ActionMailer::Base.deliveries[0].encoded)
+    assert_emails 1
+    mail = ActionMailer::Base.deliveries.first
+    assert_equal [CONFIG["email_from"]], mail.from
+    assert_equal %w(member@danbooru.com), mail.to
+    assert_equal "#{CONFIG["app_name"]} - Message received from admin", mail.subject
+    assert_equal "<p>admin said:</p>\n<div>\n  <p>Subject: hello</p>\n  <hr />\n  hello\n</div>\n", mail.body.raw_source
 
     response_a = Dmail.create(:to_name => "admin", :from_name => "member", :parent_id => msg.id, :title => "hello", :body => "you are wrong")
     assert_equal("Re: hello", response_a.title)
@@ -49,6 +54,6 @@ class DmailTest < ActiveSupport::TestCase
     ActionMailer::Base.deliveries = []
 
     Dmail.create(:to_name => "privileged", :from_name => "admin", :title => "hoge", :body => "hoge")
-    assert_equal(0, ActionMailer::Base.deliveries.size)
+    assert_no_emails
   end
 end

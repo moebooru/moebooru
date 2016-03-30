9f9f264efe287364bdcaa65efcc3aba3d4befc81
nanaya <me@myconan.net>
2016-03-30 15:42:11 +0900

    Add missed batch_uploads update

---
+++ db/migrate/20160113112901_fix_batch_uploads_created_at_default.rb
@@ -0,0 +1,9 @@
+class FixBatchUploadsCreatedAtDefault < ActiveRecord::Migration
+  def up
+    execute "ALTER TABLE batch_uploads ALTER COLUMN created_at SET DEFAULT CURRENT_TIMESTAMP"
+  end
+
+  def down
+    execute "ALTER TABLE batch_uploads ALTER COLUMN created_at SET DEFAULT CURRENT_TIMESTAMP '2010-08-31 04:17:31.209032'"
+  end
+end

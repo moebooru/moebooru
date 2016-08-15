e61944bcd78c9c3f9832df3c013db260c7dbd1e7
nanaya <me@myconan.net>
2016-08-16 05:47:34 +0900

    Slightly more useful test log

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 ENV["RAILS_ENV"] = "test"
+
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
+
+require "minitest/reporters"
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.

799375546bf62a732b2e63d7daf109a15d155c9c
nanaya <me@myconan.net>
2016-08-16 06:04:37 +0900

    Avoid using alias_method_chain

--- lib/core_ext/patch-login_check_on_links_and_buttons.rb
+++ lib/core_ext/patch-login_check_on_links_and_buttons.rb
@@ -3,36 +3,42 @@
 # example: :level => :member
 module ActionView
   module Helpers
-    module TagHelper
-      def tag_options_with_login_check(options, escape = true)
+    module TagHelperWithLoginCheck
+      def tag_options(options, escape = true)
         level = options.delete "level"
         if level && ApplicationHelper.need_signup?(level)
           options["onclick"] = "if(!User.run_login_onclick(event)) return false; #{options["onclick"] || "return true;"}"
         end
 
-        tag_options_without_login_check options, escape
+        super
       end
-      alias_method_chain :tag_options, :login_check
     end
 
-    module FormTagHelper
+    module TagHelper
+      prepend TagHelperWithLoginCheck
+    end
+
+    module FormTagHelperWithLoginCheck
       # Add the need-signup class if signing up would allow a logged-out user to
       # execute this action.  User.js uses this to determine whether it should ask
       # the user to create an account.
-      def form_tag_with_login_check(url_for_options = {}, options = {}, *parameters_for_url, &block)
+      def form_tag(url_for_options = {}, options = {}, *parameters_for_url, &block)
         level = options.delete :level
         if level && ApplicationHelper.need_signup?(level)
           options[:class] = "#{options[:class]} need-signup"
         end
 
-        form_tag_without_login_check url_for_options, options, *parameters_for_url, &block
+        super
       end
-      alias_method_chain :form_tag, :login_check
+    end
+
+    module FormTagHelper
+      prepend FormTagHelperWithLoginCheck
     end
 
     module JavaScriptHelper
       # Backported
-      def link_to_function(name, function, html_options = {})
+      def link_to_function_without_login_check(name, function, html_options = {})
         # message = "link_to_function is deprecated and will be removed from Rails 4.1. We recommend using Unobtrusive JavaScript instead. " +
         #   "See http://guides.rubyonrails.org/working_with_javascript_in_rails.html#unobtrusive-javascript"
         # ActiveSupport::Deprecation.warn message
@@ -46,7 +52,7 @@ module ActionView
       # Add the need-signup class if signing up would allow a logged-out user to
       # execute this action.  User.js uses this to determine whether it should ask
       # the user to create an account.
-      def link_to_function_with_login_check(name, *args, &block)
+      def link_to_function(name, *args, &block)
         html_options = args.extract_options!
         level = html_options.delete :level
         if level && ApplicationHelper.need_signup?(level) && args[0]
@@ -56,10 +62,9 @@ module ActionView
 
         link_to_function_without_login_check name, *args, &block
       end
-      alias_method_chain :link_to_function, :login_check
 
       # Backported
-      def button_to_function(name, function = nil, html_options = {})
+      def button_to_function_without_login_check(name, function = nil, html_options = {})
         # message = "button_to_function is deprecated and will be removed from Rails 4.1. We recommend using Unobtrusive JavaScript instead. " +
         #   "See http://guides.rubyonrails.org/working_with_javascript_in_rails.html#unobtrusive-javascript"
         # ActiveSupport::Deprecation.warn message
@@ -72,7 +77,7 @@ module ActionView
       # Add the need-signup class if signing up would allow a logged-out user to
       # execute this action.  User.js uses this to determine whether it should ask
       # the user to create an account.
-      def button_to_function_with_login_check(name, *args, &block)
+      def button_to_function(name, *args, &block)
         html_options = args.extract_options!
         level = html_options.delete :level
         if level && ApplicationHelper.need_signup?(level) && args[0]
@@ -82,7 +87,6 @@ module ActionView
 
         button_to_function_without_login_check name, *args, &block
       end
-      alias_method_chain :button_to_function, :login_check
     end
   end
 end
--- lib/core_ext/patch-remove_commit_search_from_form.rb
+++ lib/core_ext/patch-remove_commit_search_from_form.rb
@@ -1,16 +1,20 @@
 # - removes commit=Search from query url
 module ActionView
   module Helpers
-    module TagHelper
+    module TagHelperWithRemoveCommitSearch
       # submit_tag "Search" generates a submit tag that adds "commit=Search" to the URL,
       # which is ugly and unnecessary.  Override TagHelper#tag and remove this globally.
-      def tag_with_remove_commit_search(name, options = nil, open = false, escape = true)
+      def tag(name, options = nil, open = false, escape = true)
         if name == :input && options["type"] == "submit" && options["name"] == "commit" && options["value"] == "Search"
           options.delete "name"
         end
-        tag_without_remove_commit_search name, options, open, escape
+
+        super
       end
-      alias_method_chain :tag, :remove_commit_search
+    end
+
+    module TagHelper
+      prepend TagHelperWithRemoveCommitSearch
     end
   end
 end

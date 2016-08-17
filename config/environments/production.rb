a250558e2ab8c841bbacec61ff0c73dc11f0b274
nanaya <me@myconan.net>
2016-08-17 15:13:41 +0900

    Use sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :sendmail
+
   config.middleware.use ExceptionNotification::Rack,
                         :ignore_exceptions => %w(ActionController::BadRequest ActionController::ParameterMissing) + ExceptionNotifier.ignored_exceptions,
                         :email => {

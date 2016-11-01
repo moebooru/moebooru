93536974dcb7135bbe136a1006b2622962d01c8e
nanaya <me@myconan.net>
2016-11-02 05:27:58 +0900

    Totes no problem

--- config/init_config.rb
+++ config/init_config.rb
@@ -1,6 +1,6 @@
 CONFIG = {}
 # The version of this Moebooru.
-CONFIG["version"] = "5.0.0-beta.1"
+CONFIG["version"] = "5.0.0"
 
 # The default name to use for anyone who isn't logged in.
 CONFIG["default_guest_name"] = "Anonymous"

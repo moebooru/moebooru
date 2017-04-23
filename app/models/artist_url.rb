84a4f0e3b2ee5789d2b09144e8050a944cd0aac2
nanaya <me@myconan.net>
2017-04-23 21:12:09 +0900

    Normalize https

--- app/models/artist_url.rb
+++ app/models/artist_url.rb
@@ -7,8 +7,9 @@ class ArtistUrl < ActiveRecord::Base
 
     url.gsub(/^http:\/\/blog\d+\.fc2/, "http://blog.fc2")
       .gsub(/^http:\/\/blog-imgs-\d+\.fc2/, "http://blog.fc2")
-      .gsub(/^http:\/\/img\d+\.pixiv\.net/, "http://img.pixiv.net") # [1]
-      .gsub(/^http:\/\/i\d+\.pixiv\.net\/img\d+/, "http://img.pixiv.net") # [2]
+      .gsub(/^https?:\/\/img\d+\.pixiv\.net/, "http://img.pixiv.net") # [1]
+      .gsub(/^https?:\/\/i\d+\.pixiv\.net\/img\d+/, "http://img.pixiv.net") # [2]
+      .gsub(%r{\Ahttps?://www\.pixiv\.net/}, "http://www.pixiv.net/")
     # [1] pixiv pre-2012. Example URL: http://img55.pixiv.net/img/kazamatuli/16634039.jpg
     # [2] pixiv 2012. Example URL: http://i1.pixiv.net/img55/img/kazamatuli/29701368.jpg
   end
@@ -20,8 +21,9 @@ class ArtistUrl < ActiveRecord::Base
 
     url.gsub(/^http:\/\/blog\d+\.fc2/, "http://blog*.fc2")
       .gsub(/^http:\/\/blog-imgs-\d+\.fc2/, "http://blog*.fc2")
-      .gsub(/^http:\/\/img\d+\.pixiv\.net/, "http://img*.pixiv.net")
-      .gsub(/^http:\/\/i\d+\.pixiv\.net\/img\d+/, "http://img*.pixiv.net")
+      .gsub(/^https?:\/\/img\d+\.pixiv\.net/, "http://img*.pixiv.net")
+      .gsub(/^https?:\/\/i\d+\.pixiv\.net\/img\d+/, "http://img*.pixiv.net")
+      .gsub(%r{\Ahttps?://www\.pixiv\.net/}, "http://www.pixiv.net/")
   end
 
   def normalize

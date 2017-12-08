09b71629b4fb8834c6895e886db0775d84d692ea
nanaya <me@nanaya.pro>
2017-12-09 03:10:31 +0900

    Apparently I already migrated this thing

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -21,6 +21,3 @@ Rails.application.config.active_record.belongs_to_required_by_default = false
 
 # Configure SSL options to enable HSTS with subdomains. Previous versions had false.
 Rails.application.config.ssl_options = { hsts: false }
-
-# Do not halt callback chains when a callback returns false. Previous versions had true.
-ActiveSupport.halt_callback_chains_on_return_false = true

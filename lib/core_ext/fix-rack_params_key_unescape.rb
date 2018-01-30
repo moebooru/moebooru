fecb908f188a0c0b33f531060b996c6c7429fc20
nanaya <me@nanaya.pro>
2018-01-31 01:18:44 +0900

    Remove legacy check

--- lib/core_ext/fix-rack_params_key_unescape.rb
+++ lib/core_ext/fix-rack_params_key_unescape.rb
@@ -1,15 +1,13 @@
-if defined?(::Encoding)
-  require "rack/utils"
+require "rack/utils"
 
-  module Rack::Utils
-    # Refuse to unescape anything other than UTF-8.
-    def unescape(s, _encoding = "")
-      # *Might* be garbage string. Scrub.
-      URI.decode_www_form_component(s, Encoding::UTF_8).to_valid_utf8
-    rescue ArgumentError
-      # Garbage string. Scrub.
-      s.to_valid_utf8
-    end
-    module_function :unescape
+module Rack::Utils
+  # Refuse to unescape anything other than UTF-8.
+  def unescape(s, _encoding = "")
+    # *Might* be garbage string. Scrub.
+    URI.decode_www_form_component(s, Encoding::UTF_8).to_valid_utf8
+  rescue ArgumentError
+    # Garbage string. Scrub.
+    s.to_valid_utf8
   end
+  module_function :unescape
 end

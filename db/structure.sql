744e03d15f20dcc69e9c4160eeb40512197bf839
nanaya <me@nanaya.pro>
2018-12-23 00:03:01 +0900

    Extensions are not dumped anymore yay

--- db/structure.sql
+++ db/structure.sql
@@ -9,13 +9,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
 -- Name: post_status; Type: TYPE; Schema: public; Owner: -
 --
 

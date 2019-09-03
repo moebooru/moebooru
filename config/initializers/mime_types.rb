047ccfe524b25bf929ba499d33b5f7e4426a506c
nanaya <me@nanaya.pro>
2019-09-04 00:18:04 +0900

    Register txt format

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,4 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 Mime::Type.register "application/opensearchdescription+xml", :opensearch
+Mime::Type.register "text/plain", :txt

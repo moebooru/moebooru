78555979ae9e9b8f701ae48927023353e202d1d5
nanaya <me@nanaya.pro>
2020-04-05 15:59:02 +0900

    Remove erb loader from webpacker
    Too slow. Maybe put back if using spring?

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,5 @@
 const { environment } = require('@rails/webpacker')
 const coffee =  require('./loaders/coffee')
-const erb = require('./loaders/erb')
 
-environment.loaders.prepend('erb', erb)
 environment.loaders.prepend('coffee', coffee)
 module.exports = environment

605eb4909434e9a616d81de6fab9c1edfc714738
nanaya <me@nanaya.pro>
2020-05-17 20:11:36 +0900

    Fix page number parsing error when passing array

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
   def page_number
     unless @page_number
       if params[:page]
-        params[:page] = params[:page].to_i
+        params[:page] = params[:page].try(:to_i) || 1
         params.delete(:page) if params[:page] <= 1
       end
       @page_number = params[:page] || 1

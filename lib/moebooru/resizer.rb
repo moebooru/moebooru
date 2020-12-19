8dc5282d4bedbc482f01a59ab871043587f4a952
nanaya <me@nanaya.pro>
2020-12-19 18:28:47 +0900

    Save images in progressive mode

--- lib/moebooru/resizer.rb
+++ lib/moebooru/resizer.rb
@@ -52,6 +52,7 @@ module Moebooru
         convert.crop write_crop
         convert.resize write_size
         convert.repage.+
+        convert.interlace 'Line'
 
         if write_format =~ /\Ajpe?g\z/
           convert.sampling_factor "2x2,1x1,1x1"

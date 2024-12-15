5f33751290d9168b824736b13c56137a9dc7cd2a
nanaya <me@nanaya.net>
2024-12-15 21:22:37 +0900

    Javascript math utils
    Remove prototypejs gsub usage while at it.

--- app/javascript/src/utils/math.js
+++ app/javascript/src/utils/math.js
@@ -1,25 +1,29 @@
-export clamp = (n, min, max) ->
-  Math.max Math.min(n, max), min
+export function clamp (n, min, max) {
+  return Math.max(Math.min(n, max), min);
+}
 
-# Return the squared distance between two points.
-export distanceSquared = (x1, y1, x2, y2) ->
-  (x1 - x2) ** 2 + (y1 - y2) ** 2
+// Return the squared distance between two points.
+export function distanceSquared (x1, y1, x2, y2) {
+  return (x1 - x2) ** 2 + (y1 - y2) ** 2;
+}
 
-export numberToHumanSize = (size, precision) ->
-  precision ?= 1
-  text = undefined
-  size = Number(size)
-  if size.toFixed(0) == '1'
-    text = '1 Byte'
-  else if size < 1024
-    text = size.toFixed(0) + ' Bytes'
-  else if size < 1024 * 1024
-    text = (size / 1024).toFixed(precision) + ' KB'
-  else if size < 1024 * 1024 * 1024
-    text = (size / (1024 * 1024)).toFixed(precision) + ' MB'
-  else if size < 1024 * 1024 * 1024 * 1024
-    text = (size / (1024 * 1024 * 1024)).toFixed(precision) + ' GB'
-  else
-    text = (size / (1024 * 1024 * 1024 * 1024)).toFixed(precision) + ' TB'
+export function numberToHumanSize (size, precision) {
+  precision ??= 1;
+  size = Number(size);
+  let text;
+  if (size.toFixed(0) === '1') {
+    text = '1 Byte';
+  } else if (size < 1024) {
+    text = `${size.toFixed(0)} Bytes`;
+  } else if (size < 1024 * 1024) {
+    text = `${(size / 1024).toFixed(precision)} KB`;
+  } else if (size < 1024 * 1024 * 1024) {
+    text = `${(size / (1024 * 1024)).toFixed(precision)} MB`;
+  } else if (size < 1024 * 1024 * 1024 * 1024) {
+    text = `${(size / (1024 * 1024 * 1024)).toFixed(precision)} GB`;
+  } else {
+    text = `${(size / (1024 * 1024 * 1024 * 1024)).toFixed(precision)} TB`;
+  }
 
-  text.gsub(/([0-9]\.\d*?)0+ /, '#{1} ').gsub(/\. /, ' ')
+  return text.replace(/([0-9]\.\d*?)0+ /, '$1 ').replace('. ', ' ');
+}

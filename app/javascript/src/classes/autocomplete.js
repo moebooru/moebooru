6dfa49262ae559f373dc16d3d68fa0c996648087
nanaya <me@nanaya.net>
2025-01-21 02:27:23 +0900

    Javascript Autocomplete (convert)

--- app/javascript/src/classes/autocomplete.js
+++ app/javascript/src/classes/autocomplete.js
@@ -1,48 +1,77 @@
-import autocompleter from 'autocompleter'
-import TagCompletion from './tag_completion'
-import TagCompletionBox from './tag_completion_box'
+var $, Autocomplete, autocompleterMap;
 
-$ = jQuery
+import autocompleter from 'autocompleter';
 
-autocompleterMap = (match) =>
-  label: match
-  value: match
+import TagCompletion from './tag_completion';
 
-export default class Autocomplete
-  constructor: (@tagCompletionInstance) ->
-    $ =>
-      @_genericCompletionAll()
-      @_tagCompletion()
+import TagCompletionBox from './tag_completion_box';
 
+$ = jQuery;
 
-  _genericCompletion: (input) =>
-    url = input.dataset.autocomplete
-    autocompleter
-      input: input
-      fetch: (text, update) =>
-        $.ajax url,
-          data:
+autocompleterMap = (match) => {
+  return {
+    label: match,
+    value: match
+  };
+};
+
+export default Autocomplete = class Autocomplete {
+  constructor(tagCompletionInstance) {
+    this._genericCompletion = this._genericCompletion.bind(this);
+    this._genericCompletionAll = this._genericCompletionAll.bind(this);
+    this._tagCompletion = this._tagCompletion.bind(this);
+    this.tagCompletionInstance = tagCompletionInstance;
+    $(() => {
+      this._genericCompletionAll();
+      return this._tagCompletion();
+    });
+  }
+
+  _genericCompletion(input) {
+    var url;
+    url = input.dataset.autocomplete;
+    return autocompleter({
+      input: input,
+      fetch: (text, update) => {
+        return $.ajax(url, {
+          data: {
             term: text
+          },
           dataType: 'json'
-        .done (matches) =>
-          update matches.map(autocompleterMap)
-      onSelect: (match) => input.value = match.value
-      preventSubmit: 2 # OnSelect
+        }).done((matches) => {
+          return update(matches.map(autocompleterMap));
+        });
+      },
+      onSelect: (match) => {
+        return input.value = match.value;
+      },
+      preventSubmit: 2 // OnSelect
+    });
+  }
 
+  _genericCompletionAll() {
+    var i, input, len, ref, results;
+    ref = document.querySelectorAll('[data-autocomplete]');
+    results = [];
+    for (i = 0, len = ref.length; i < len; i++) {
+      input = ref[i];
+      results.push(this._genericCompletion(input));
+    }
+    return results;
+  }
 
-  _genericCompletionAll: =>
-    for input in document.querySelectorAll('[data-autocomplete]')
-      @_genericCompletion(input)
+  _tagCompletion() {
+    var editForm, tags;
+    editForm = document.querySelector('#edit-form');
+    if (editForm == null) {
+      return;
+    }
+    tags = document.querySelector('.ac-tags');
+    if (tags == null) {
+      return;
+    }
+    new TagCompletionBox(tags);
+    return this.tagCompletionInstance.observe_tag_changes_on_submit(editForm, tags);
+  }
 
-
-  _tagCompletion: =>
-    editForm = document.querySelector('#edit-form')
-
-    return if !editForm?
-
-    tags = document.querySelector('.ac-tags')
-
-    return if !tags?
-
-    new TagCompletionBox(tags)
-    @tagCompletionInstance.observe_tag_changes_on_submit editForm, tags
+};

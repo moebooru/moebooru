d9c09e855fc354dc2e599edf51b0f24ef9fc6ca5
nanaya <me@nanaya.net>
2026-06-17 04:55:20 +0900

    Javascript forum component

--- app/javascript/src/forum.js
+++ app/javascript/src/forum.js
@@ -1,30 +1,31 @@
-(($, t) ->
-  window.Forum =
-    mark_all_read: ->
-      $.ajax(url: Moebooru.path('/forum/mark_all_read')).done ->
-        $('span.forum-topic').removeClass 'unread-topic'
-        $('div.forum-update').removeClass 'forum-update'
-        menu.syncForumMenu(true)
-        notice t('.mark_as_read')
-        return
-      return
-    quote: (id) ->
-      $.ajax(
-        url: Moebooru.path('/forum/show.json')
-        type: 'get'
-        data: 'id': id).done((resp) ->
-        stripped_body = resp.body.replace(/\[quote\](?:.|\n|\r)+?\[\/quote\][\n\r]*/gm, '')
-        $('#reply').show()
-        $('#forum_post_body').val (i, val) ->
-          val + '[quote]' + resp.creator + ' ' + t('js.said') + '\n' + stripped_body + '\n[/quote]\n\n'
-        if $('#respond-link')
-          $('#respond-link').hide()
-        if $('#forum_post_body')
-          $('#forum_post_body').focus()
-        return
-      ).fail ->
-        notice t('.quote_error')
-        return
-      return
-  return
-) jQuery, I18n.scopify('js.forum')
+/* global Moebooru, menu, notice, jQuery, I18n */
+const $ = jQuery;
+const t = I18n.scopify('js.forum');
+
+window.Forum = {
+  mark_all_read () {
+    $.ajax(Moebooru.path('/forum/mark_all_read')).done(function () {
+      $('span.forum-topic').removeClass('unread-topic');
+      $('div.forum-update').removeClass('forum-update');
+      menu.syncForumMenu(true);
+      notice(t('.mark_as_read'));
+    });
+  },
+  quote (id) {
+    $.ajax({
+      url: Moebooru.path('/forum/show.json'),
+      type: 'get',
+      data: { id }
+    }).done(function (resp) {
+      const strippedBody = resp.body.replace(/\[quote\](?:.|\n|\r)+?\[\/quote\][\n\r]*/gm, '');
+      $('#reply').show();
+      $('#forum_post_body').val((i, val) => (
+        val + '[quote]' + resp.creator + ' ' + t('js.said') + '\n' + strippedBody + '\n[/quote]\n\n'
+      ));
+      $('#respond-link')?.hide();
+      $('#forum_post_body')?.focus();
+    }).fail(function () {
+      notice(t('.quote_error'));
+    });
+  }
+};

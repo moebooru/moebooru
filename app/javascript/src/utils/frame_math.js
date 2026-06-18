6aaa9709a94b5e82fd526dfdb29a93cb81c03ec3
nanaya <me@nanaya.net>
2026-06-19 02:14:16 +0900

    Javascript frame math utils

--- app/javascript/src/utils/frame_math.js
+++ app/javascript/src/utils/frame_math.js
@@ -1,21 +1,26 @@
-# Convert dimensions scaled to an image back to the source resolution.
-export frameDimensionsFromImage = (frame, image, post) ->
-  targetScale = post.width / image.width
-  scale = (source) -> Math.round(source * targetScale)
+// Convert dimensions scaled to an image back to the source resolution.
+export function frameDimensionsFromImage (frame, image, post) {
+  const targetScale = post.width / image.width;
+  const scale = (source) => Math.round(source * targetScale);
 
-  source_top: scale frame.top
-  source_left: scale frame.left
-  source_width: scale frame.width
-  source_height: scale frame.height
+  return {
+    source_top: scale(frame.top),
+    source_left: scale(frame.left),
+    source_width: scale(frame.width),
+    source_height: scale(frame.height)
+  };
+}
 
+// Given a frame, its post and an image, return the frame's rectangle scaled to
+// the size of the image.
+export function frameDimensionsToImage (frame, image, post) {
+  const targetScale = image.width / post.width;
+  const scale = (source) => Math.round(source * targetScale);
 
-# Given a frame, its post and an image, return the frame's rectangle scaled to
-# the size of the image.
-export frameDimensionsToImage = (frame, image, post) ->
-  targetScale = image.width / post.width
-  scale = (source) -> Math.round(source * targetScale)
-
-  top: scale frame.source_top
-  left: scale frame.source_left
-  width: scale frame.source_width
-  height: scale frame.source_height
+  return {
+    top: scale(frame.source_top),
+    left: scale(frame.source_left),
+    width: scale(frame.source_width),
+    height: scale(frame.source_height)
+  };
+}

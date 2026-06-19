22de39493dbe9987ebb801c6fc528e6ef4766fc2
nanaya <me@nanaya.net>
2026-06-20 02:26:42 +0900

    Javascript I18n helpers

--- app/javascript/src/i18n.js
+++ app/javascript/src/i18n.js
@@ -1,8 +1,14 @@
-I18n.defaultLocale = locale.default
-I18n.locale = locale.current
+/* globals I18n, locale */
+function scopify (scope) {
+  return function (label, options) {
+    if (label[0] === '.') {
+      label = `${scope}${label}`;
+    }
 
-I18n.scopify = (scope) ->
-  (label, options) ->
-    if label[0] == '.'
-      label = "#{scope}#{label}"
-    I18n.t label, options
+    return I18n.t(label, options);
+  };
+}
+
+I18n.defaultLocale = locale.default;
+I18n.locale = locale.current;
+I18n.scopify = scopify;

eb98c8fbc112a50b165557c810e73db219bee720
nanaya <me@nanaya.net>
2026-06-21 05:07:18 +0900

    Javascript Notice class

--- app/javascript/src/classes/notice.js
+++ app/javascript/src/classes/notice.js
@@ -1,39 +1,43 @@
-$ = jQuery
+/* globals jQuery */
+import { Cookies } from 'src/cookie';
 
+const $ = jQuery;
 
-export default class Notice
-  constructor: ->
-    $ @initialize
+export default class Notice {
+  constructor () {
+    $(this.initialize);
+  }
 
+  hide () {
+    $('#notice-container').hide();
+  }
 
-  hide: =>
-    $('#notice-container').hide()
+  initialize = () => {
+    const msg = Cookies.get('notice');
 
+    if (msg == null || msg === '') return;
 
-  initialize: =>
-    msg = Cookies.get 'notice'
+    this.show(msg, true);
+    Cookies.remove('notice');
+  };
 
-    return unless msg? && msg != ''
+  // If initial is true, this is a notice set by the notice cookie and not a
+  // realtime notice from user interaction.
+  show = (msg, initial) => {
+    // If this is an initial notice, and this screen has a dedicated notice
+    // container other than the floating notice, use that and don't hide it.
+    if (initial ?? false) {
+      const $staticNotice = $('#static_notice');
+      if ($staticNotice.length > 0) {
+        $staticNotice.text(msg).show();
+        return;
+      }
+    }
 
-    @show msg, true
-    Cookies.remove 'notice'
+    $('#notice').text(msg);
+    $('#notice-container').show();
 
-
-  # If initial is true, this is a notice set by the notice cookie and not a
-  # realtime notice from user interaction.
-  show: (msg, initial) =>
-    # If this is an initial notice, and this screen has a dedicated notice
-    # container other than the floating notice, use that and don't hide it.
-    if initial ? false
-      $staticNotice = $('#static_notice')
-      if $staticNotice.length > 0
-        $staticNotice
-          .text(msg)
-          .show()
-        return
-
-    $('#notice').text msg
-    $('#notice-container').show()
-
-    clearTimeout @timeout
-    @timeout = setTimeout @hide, 5000
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(this.hide, 5000);
+  };
+}

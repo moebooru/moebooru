8e8f96a06ec788b8b5469e503db8fa3203385fd6
nanaya <me@nanaya.net>
2026-06-21 04:35:51 +0900

    Javascript moebooru component

--- app/javascript/src/moebooru.js
+++ app/javascript/src/moebooru.js
@@ -1,53 +1,61 @@
-$ = jQuery
-window.Moebooru = {}
-window.Moe = $(Moebooru)
+/* globals jQuery, Vars */
+const $ = jQuery;
+window.Moebooru = {};
+window.Moe = $(window.Moebooru);
 
-Moebooru.path = (url) ->
-  if Vars.prefix == '/' then url else "#{Vars.prefix}#{url}"
+window.Moebooru.path = (url) => (
+  Vars.prefix === '/' ? url : `${Vars.prefix}${url}`
+);
 
-# XXX: Tested on chrome, mozilla, msie(9/10)
-# might or might not work in other browser
-Moebooru.dragElement = (el) ->
-  win = $(window)
-  doc = $(document)
-  prevPos = []
+// XXX: Tested on chrome, mozilla, msie(9/10)
+// might or might not work in other browser
+window.Moebooru.dragElement = (el) => {
+  const doc = $(document);
+  let prevPos = [];
 
-  current = (x, y) ->
-    windowOffset = [
-      window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft
+  function current (x, y) {
+    const windowOffset = [
+      window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
       window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
-    ]
-    offset = [
-      windowOffset[0] + prevPos[0] - x
+    ];
+    const offset = [
+      windowOffset[0] + prevPos[0] - x,
       windowOffset[1] + prevPos[1] - y
-    ]
-    prevPos[0] = x
-    prevPos[1] = y
-    offset
+    ];
 
-  el.on 'dragstart', ->
-    false
+    prevPos[0] = x;
+    prevPos[1] = y;
 
-  el.on 'mousedown', (e) ->
-    return unless e.which == 1
+    return offset;
+  }
 
-    pageScroller = (e) ->
-      scroll = current(e.clientX, e.clientY)
-      scrollTo scroll[0], scroll[1]
-      el.attr 'data-drag-element', '1'
-      false
+  el.on('dragstart', () => {
+    return false;
+  });
 
-    unsetAttr = ->
-      el.removeAttr 'data-drag-element'
+  el.on('mousedown', (e) => {
+    if (e.which !== 1) return;
 
-    el.css 'cursor', 'pointer'
-    prevPos = [e.clientX, e.clientY]
+    const pageScroller = (e) => {
+      window.scrollTo(...current(e.clientX, e.clientY));
+      el.attr('data-drag-element', '1');
+      return false;
+    };
 
-    doc.on 'mousemove', pageScroller
+    el.css('cursor', 'pointer');
+    prevPos = [e.clientX, e.clientY];
 
-    doc.one 'mouseup', (e) ->
-      doc.off 'mousemove', pageScroller
-      setTimeout unsetAttr, 0
-      el.css 'cursor', 'auto'
-      false
-    false
+    doc.on('mousemove', pageScroller);
+
+    doc.one('mouseup', (e) => {
+      doc.off('mousemove', pageScroller);
+      setTimeout(() => {
+        el.removeAttr('data-drag-element');
+      }, 0);
+      el.css('cursor', 'auto');
+      return false;
+    });
+
+    return false;
+  });
+};
